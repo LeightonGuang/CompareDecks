010b0b37b6b1ea1ffa32f52a30e7067a67423b8c
LeightonGuang <leighton.guang@icloud.com>
2024-07-22 06:02:15 +0100

    feat: centering hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main className="min-h-[100dvh]">
-      <div className="mx-[1rem] mt-mobile-spacing">
+      <div className="flex justify-center mx-[1rem] mt-mobile-spacing xl:mx-desktop-spacing max-w[84.5rem]">
         <Hero />
       </div>
     </main>
