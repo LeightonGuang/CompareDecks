fa2b974e5f135014f1aa849e5afbc3b371a9a0cb
LeightonGuang <leighton.guang@icloud.com>
2024-07-23 05:05:52 +0100

    feat: fix head nav spacings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className=" bg-red-600">
-      <div className="flex mx-mobile-spacing py-mobile-spacing justify-between items-center">
+      <div className="flex mx-mobile-spacing xl:mx-[1.5rem] py-mobile-spacing justify-between items-center">
         <img
           className="h-mobile-spacing"
           src="https://thumbs.dreamstime.com/b/temporary-rubber-stamp-over-white-background-86664158.jpg"
           alt="logo"
         />
-        <nav className="flex gap-mobile-spacing bg-red">
+        <nav className="flex gap-[1.5rem] bg-red font-[.875rem] font-medium">
           <Link href={"/"}>Home</Link>
           <Link href={"/setting"}>Setting</Link>
         </nav>
