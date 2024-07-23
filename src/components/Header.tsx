4e59b5fd51d8282510d22d8dbeb472ff4c0da0a2
LeightonGuang <leighton.guang@icloud.com>
2024-07-23 21:12:45 +0100

    feat: logo links to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,13 @@ const Header = () => {
   return (
     <div className=" bg-red-600">
       <div className="flex mx-mobile-spacing xl:mx-[1.5rem] py-mobile-spacing justify-between items-center">
-        <img
-          className="h-mobile-spacing"
-          src="https://thumbs.dreamstime.com/b/temporary-rubber-stamp-over-white-background-86664158.jpg"
-          alt="logo"
-        />
+        <Link href={"/"}>
+          <img
+            className="h-mobile-spacing"
+            src="https://thumbs.dreamstime.com/b/temporary-rubber-stamp-over-white-background-86664158.jpg"
+            alt="logo"
+          />
+        </Link>
         <nav className="flex gap-[1.5rem] bg-red font-[.875rem] font-medium">
           <Link href={"/"}>Home</Link>
           <Link href={"/setting"}>Setting</Link>
