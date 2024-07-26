09d5b3f81192827125df75691adcba0c3538c360
LeightonGuang <leighton.guang@icloud.com>
2024-07-26 07:14:54 +0100

    feat: change deckData structure and fix issues with unpin button

--- src/components/compareList/CompareList.tsx
+++ src/components/compareList/CompareList.tsx
@@ -4,18 +4,14 @@ import { useState } from "react";
 import CompareCard from "./CompareCard";
 import editIcon from "../../_assets/icons/editIcon.svg";
 import Image from "next/image";
-import { CardType } from "@/_types/CardType";
 
-const CompareList = ({
-  deckName,
-  deckData,
-}: {
-  deckName: string;
-  deckData: any[];
-}) => {
-  const orderedList: CardType[] = deckData;
+import { CardType } from "@/_types/CardType";
+import { DeckType } from "@/_types/DeckType";
+
+const CompareList = ({ deckData }: { deckData: DeckType }) => {
+  const orderedList: CardType[] = deckData.cards;
   const [pinnedList, setPinnedList] = useState<CardType[]>([]);
-  const [unpinnedList, setUnpinnedList] = useState<CardType[]>(deckData);
+  const [unpinnedList, setUnpinnedList] = useState<CardType[]>(deckData.cards);
 
   const handlePinButton: (objIndex: number) => void = (objIndex) => {
     setPinnedList([...pinnedList, unpinnedList[objIndex]]);
@@ -24,16 +20,21 @@ const CompareList = ({
     );
   };
 
-  const handleUnpinButton: (objIndex: number) => void = (objIndex) => {
-    for (let i: number = 0; i < orderedList.length; i++) {
-      const originalEl: CardType = orderedList[i];
+  const handleUnpinButton = (objIndex: number) => {
+    const itemToUnpin = pinnedList[objIndex];
+    setPinnedList((prevList) =>
+      prevList.filter((_obj, index) => index !== objIndex)
+    );
 
-      if (pinnedList[objIndex] === originalEl) {
-        unpinnedList.splice(i, 0, pinnedList[objIndex]);
-        setPinnedList(pinnedList.filter((_obj, index) => index !== objIndex));
-        break;
-      }
-    }
+    const originalIndex = orderedList.findIndex(
+      (item) => item.id === itemToUnpin.id
+    );
+
+    setUnpinnedList((prevList) => {
+      const newList = [...prevList];
+      newList.splice(originalIndex, 0, itemToUnpin);
+      return newList;
+    });
   };
 
   return (
@@ -42,7 +43,9 @@ const CompareList = ({
       id="compare-list"
     >
       <div className="flex gap-[0.5rem] items-center">
-        <h2 className="font-bold text-[1.5rem] leading-[2rem]">{deckName}</h2>
+        <h2 className="font-bold text-[1.5rem] leading-[2rem]">
+          {deckData.name}
+        </h2>
         <button className="mx-[0.625rem]">
           <Image src={editIcon} alt="edit icon" height={20} width={20} />
         </button>
@@ -57,11 +60,11 @@ const CompareList = ({
             key={cardIndex}
           >
             <CompareCard
+              isPinned={true}
               cardObj={cardObj}
+              cardIndex={cardIndex}
               handlePinButton={handlePinButton}
               handleUnpinButton={handleUnpinButton}
-              cardIndex={cardIndex}
-              isPinned={true}
             />
           </li>
         ))}
@@ -71,11 +74,11 @@ const CompareList = ({
             key={cardIndex}
           >
             <CompareCard
+              isPinned={false}
               cardObj={cardObj}
+              cardIndex={cardIndex}
               handlePinButton={handlePinButton}
               handleUnpinButton={handleUnpinButton}
-              cardIndex={cardIndex}
-              isPinned={false}
             />
           </li>
         ))}
