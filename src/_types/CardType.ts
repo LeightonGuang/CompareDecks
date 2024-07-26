a7ff64b25eb0384d7b382ecdd597765efed048b1
LeightonGuang <leighton.guang@icloud.com>
2024-07-26 07:14:03 +0100

    feat: update card type

--- src/_types/CardType.ts
+++ src/_types/CardType.ts
@@ -1,8 +1,12 @@
 export interface CardType {
+  id: number;
+  deck_uuid: string;
   imgUrl: string;
   brand: string;
   name: string;
   year: number;
   price: string;
   description: string;
+  created_at: string;
+  edited_at: string;
 }
