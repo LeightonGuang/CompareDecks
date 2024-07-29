a965037e8f6693e5379c9d59649ba65de34cce0b
LeightonGuang <leighton.guang@icloud.com>
2024-07-29 08:24:48 +0100

    feat: compare list to accept empty deckData prop

--- src/components/compareList/CompareList.tsx
+++ src/components/compareList/CompareList.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { useFormStatus } from "react-dom";
 import CompareCard from "./CompareCard";
 import editIcon from "../../_assets/icons/editIcon.svg";
 import Image from "next/image";
@@ -9,12 +8,15 @@ import Image from "next/image";
 import { CardType } from "@/_types/CardType";
 import { DeckType } from "@/_types/DeckType";
 
-const CompareList = ({ deckData }: { deckData: DeckType }) => {
-  const orderedList: CardType[] = deckData.cards;
+const CompareList = ({ deckData }: { deckData?: DeckType | null }) => {
+  const orderedList: CardType[] = deckData?.cards || [];
   const [pinnedList, setPinnedList] = useState<CardType[]>([]);
-  const [unpinnedList, setUnpinnedList] = useState<CardType[]>(deckData.cards);
-  const [deckName, setDeckName] = useState<string>(deckData.name);
+  const [unpinnedList, setUnpinnedList] = useState<CardType[]>(
+    deckData?.cards || []
+  );
+  const [deckName, setDeckName] = useState<string>(deckData?.name || "");
   const [isEditDeckName, setIsEditDeckName] = useState<boolean>(false);
+  const [isAddCardModal, setIsAddCardModal] = useState<boolean>(false);
 
   const handlePinButton: (objIndex: number) => void = (objIndex) => {
     setPinnedList([...pinnedList, unpinnedList[objIndex]]);
@@ -53,24 +55,38 @@ const CompareList = ({ deckData }: { deckData: DeckType }) => {
     setIsEditDeckName(false);
   };
 
+  const handleAddCardButton = () => {
+    setIsAddCardModal(true);
+  };
+
+  const handleEditCardButton = () => {};
+
+  const handleDeleteCardButton = () => {};
+
   return (
     <div
       className="pl-mobile-spacing pt-mobile-spacing pr-mobile-spacing"
       id="compare-list"
     >
-      <div className="flex gap-[0.5rem] items-center">
+      <div className="flex gap-[0.5rem] items-center" id="compare-list-header">
         {isEditDeckName ? (
-          <>
-            <form className="flex gap-[0.5rem]" onSubmit={onSubmitDeckName}>
-              <input
-                className="border rounded-[0.375rem]"
-                type="text"
-                value={deckName}
-                onChange={onChangeDeckName}
-              />
-              <button type="submit">Save</button>
-            </form>
-          </>
+          <form
+            className="flex gap-[1rem] h-[2rem]"
+            onSubmit={onSubmitDeckName}
+          >
+            <input
+              className="border w-[12rem] rounded-[0.375rem]"
+              type="text"
+              value={deckName}
+              onChange={onChangeDeckName}
+            />
+            <button
+              className="bg-[#2563eb] px-[0.5rem] py-[0.25rem] text-[0.75rem] text-white rounded-[0.375rem]"
+              type="submit"
+            >
+              Save
+            </button>
+          </form>
         ) : (
           <>
             <h2 className="font-bold text-[1.5rem] leading-[2rem]">
@@ -97,6 +113,8 @@ const CompareList = ({ deckData }: { deckData: DeckType }) => {
               cardIndex={cardIndex}
               handlePinButton={handlePinButton}
               handleUnpinButton={handleUnpinButton}
+              handleEditCardButton={handleEditCardButton}
+              handleDeleteCardButton={handleDeleteCardButton}
             />
           </li>
         ))}
@@ -106,15 +124,44 @@ const CompareList = ({ deckData }: { deckData: DeckType }) => {
             key={cardIndex}
           >
             <CompareCard
-              isPinned={false}
+              isPinned={true}
               cardObj={cardObj}
               cardIndex={cardIndex}
               handlePinButton={handlePinButton}
               handleUnpinButton={handleUnpinButton}
+              handleEditCardButton={handleEditCardButton}
+              handleDeleteCardButton={handleDeleteCardButton}
             />
           </li>
         ))}
+        <li className="w-1/2 px-[0.25rem] flex-shrink-0 mb-mobile-spacing snap-start md:w-1/4 xl:w-1/5">
+          <div className="h-[2.5rem] mb-[1rem]" />
+          <button
+            className="flex items-center justify-center h-max w-full border border-1 border-[#e2e8f0] bg-[#f8fafc] text-[3rem] font-[700] rounded-[0.5rem]"
+            onClick={handleAddCardButton}
+          >
+            <p className="text-[#64748b]">+</p>
+          </button>
+        </li>
       </ul>
+
+      {isAddCardModal ? (
+        <div
+          className="flex items-center justify-center fixed top-0 left-0 w-full h-full bg-black bg-opacity-50"
+          id="add-card-modal"
+        >
+          <div id="add-card">
+            <button
+              className="bg-white"
+              onClick={() => setIsAddCardModal(false)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      ) : (
+        ``
+      )}
     </div>
   );
 };
