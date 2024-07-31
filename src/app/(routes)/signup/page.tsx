b18128a9d87f74208f4a6ebb7c69984a01df7dd6
LeightonGuang <leighton.guang@icloud.com>
2024-07-31 06:34:03 +0100

    feat: working signup

--- src/app/(routes)/signup/page.tsx
+++ src/app/(routes)/signup/page.tsx
@@ -1,4 +1,6 @@
-const Signup = () => {
+import { signup } from "@/app/signup/actions";
+
+const Signup = async () => {
   return (
     <main className="flex justify-center items-center h-dynamic-vh overflow-y-auto">
       <div className="p-[2rem]" id="sign-up-container">
@@ -11,10 +13,20 @@ const Signup = () => {
           </p>
           <form className="flex flex-col gap-[1rem] text-left">
             <label className="flex flex-col text-[0.875rem] font-[500]">
+              Username
+              <input
+                className="border border-[#E2E8F0] font-[0.875rem] py-[0.5rem] px-[0.75rem] mt-[0.5rem] rounded-[0.375rem]"
+                placeholder="John Smith"
+                name="displayName"
+                type="text"
+              />
+            </label>
+            <label className="flex flex-col text-[0.875rem] font-[500]">
               Email
               <input
                 className="border border-[#E2E8F0] font-[0.875rem] py-[0.5rem] px-[0.75rem] mt-[0.5rem] rounded-[0.375rem]"
                 placeholder="m@example.com"
+                name="email"
                 type="text"
               />
             </label>
@@ -22,6 +34,7 @@ const Signup = () => {
               Password
               <input
                 className="border border-[#E2E8F0] font-[0.875rem] py-[0.5rem] px-[0.75rem] mt-[0.5rem] rounded-[0.375rem]"
+                name="password"
                 type="password"
               />
             </label>
@@ -29,12 +42,14 @@ const Signup = () => {
               Confirm Password
               <input
                 className="border border-[#E2E8F0] font-[0.875rem] py-[0.5rem] px-[0.75rem] mt-[0.5rem] rounded-[0.375rem]"
+                name="confirmPassword"
                 type="password"
               />
             </label>
 
             <button
               className="bg-blue text-[#f8f8fc] py-[0.5rem] px-[1rem] rounded-[0.375rem]"
+              formAction={signup}
               type="submit"
             >
               Sign Up
@@ -45,5 +60,4 @@ const Signup = () => {
     </main>
   );
 };
-
 export default Signup;
