a10eece75a5427d90325b08411c6274ad59142f3
LeightonGuang <leighton.guang@icloud.com>
2024-08-01 01:50:35 +0100

    feat: only load header when user state is set

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,50 +2,56 @@
 
 import { logout } from "@/app/logout/actions";
 import getUser from "@/utils/getUser";
-import { createClient } from "@/utils/supabase/client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [user, setUser] = useState<any>(null);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchUser = async () => {
       const user = await getUser();
       setUser(user);
+      setIsLoading(false);
     };
     fetchUser();
   }, []);
 
   return (
-    <div className="bg-white">
-      <div className="flex mx-mobile-spacing xl:mx-[1.5rem] justify-between items-center leading-[1.25rem]">
-        <Link href={"/"}>
-          <img
-            className="h-[2.5rem]"
-            src="https://thumbs.dreamstime.com/b/temporary-rubber-stamp-over-white-background-86664158.jpg"
-            alt="logo"
-          />
-        </Link>
-        <nav className="flex gap-[1.5rem] py-mobile-spacing font-[.875rem] font-medium">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/create-deck"}>Create Deck</Link>
-          <Link href={"/decks"}>Browse</Link>
-          {user && user.aud === "authenticated" ? (
-            <>
-              <Link href={"/setting"}>Setting</Link>
-              <Link href={"/account"}>Account</Link>
-              <form action={logout}>
-                <button type="submit">Logout</button>
-              </form>
-            </>
-          ) : (
-            <>
-              <Link href={"/login"}>Log in</Link>
-            </>
-          )}
-        </nav>
-      </div>
+    <div className="bg-white max-w-full">
+      {isLoading ? (
+        ``
+      ) : (
+        <div className="flex mx-mobile-spacing xl:mx-[1.5rem] justify-between items-center leading-[1.25rem]">
+          <Link href={"/"}>
+            <img
+              className="h-[2.5rem]"
+              src="https://thumbs.dreamstime.com/b/temporary-rubber-stamp-over-white-background-86664158.jpg"
+              alt="logo"
+            />
+          </Link>
+          <nav className="flex gap-[1.5rem] py-mobile-spacing font-[.875rem] font-medium">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/create-deck"}>Create Deck</Link>
+            <Link href={"/decks"}>Browse</Link>
+            {user && user.aud === "authenticated" ? (
+              <>
+                <Link href={"/setting"}>Setting</Link>
+                <Link href={"/account"}>Account</Link>
+                <form action={logout}>
+                  <button type="submit">Logout</button>
+                </form>
+              </>
+            ) : (
+              <>
+                <Link href={"/login"}>Log in</Link>
+              </>
+            )}
+          </nav>
+        </div>
+      )}
     </div>
   );
 };
