e8f40122c2845f71d5ce98c9707777c4d9b0cbff
LeightonGuang <leighton.guang@icloud.com>
2024-08-02 09:48:44 +0100

    feat: new setting page

---
+++ src/app/(routes)/setting/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SettingPage = () => {
+  return (
+    <main className="h-dynamic-vh overflow-y-auto" id="setting-page">
+      <div id="setting-container">
+        <h1>Setting</h1>
+      </div>
+    </main>
+  );
+};
+
+export default SettingPage;
