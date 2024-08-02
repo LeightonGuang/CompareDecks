3dc467d10fc7106f672c347bef0e325c144c4941
LeightonGuang <leighton.guang@icloud.com>
2024-08-02 09:22:32 +0100

    feat: drop down menu for account

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,41 @@
 import { logout } from "@/app/logout/actions";
 import getUser from "@/utils/getUser";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 const Header = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [user, setUser] = useState<any>(null);
+
+  const NavLink: FC<{ href: string; children: React.ReactNode }> = ({
+    href,
+    children,
+  }) => (
+    <Link href={href}>
+      <div className="whitespace-nowrap hover:underline">{children}</div>
+    </Link>
+  );
+
+  const GuestLinks: FC = () => <NavLink href="/login">Log in</NavLink>;
+
+  const UserLinks: FC = () => (
+    <div className="group relative inline-block">
+      <button className="hover:underline">Account</button>
+      <div
+        className="absolute right-0 top-[2rem] z-10 hidden flex-col group-focus-within:block"
+        id="dropdown-card"
+      >
+        <div className="flex flex-col gap-[1rem]">
+          <NavLink href="/setting">Setting</NavLink>
+          <form action={logout}>
+            <button className="hover:underline" type="submit">
+              Logout
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 
   useEffect(() => {
     setIsLoading(true);
@@ -20,11 +50,14 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="bg-white max-w-full">
+    <div className="max-w-full bg-white">
       {isLoading ? (
         ``
       ) : (
-        <div className="flex mx-mobile-spacing xl:mx-[1.5rem] justify-between items-center leading-[1.25rem]">
+        <div
+          className="mx-mobile-spacing flex max-h-[3.25rem] items-center justify-between leading-[1.25rem] xl:mx-[1.5rem]"
+          id="header"
+        >
           <Link href={"/"}>
             <img
               className="h-[2.5rem]"
@@ -33,21 +66,14 @@ const Header = () => {
             />
           </Link>
           <nav className="flex gap-[1.5rem] py-mobile-spacing font-[.875rem] font-medium">
-            <Link href={"/"}>Home</Link>
-            <Link href={"/create-deck"}>Create Deck</Link>
-            <Link href={"/decks"}>Browse</Link>
+            <NavLink href="/create-deck">Create Deck</NavLink>
+            <NavLink href={"/decks"}>Browse</NavLink>
+          </nav>
+          <nav className="flex gap-[1.5rem] py-mobile-spacing font-[.875rem] font-medium">
             {user && user.aud === "authenticated" ? (
-              <>
-                <Link href={"/setting"}>Setting</Link>
-                <Link href={"/account"}>Account</Link>
-                <form action={logout}>
-                  <button type="submit">Logout</button>
-                </form>
-              </>
+              <UserLinks />
             ) : (
-              <>
-                <Link href={"/login"}>Log in</Link>
-              </>
+              <GuestLinks />
             )}
           </nav>
         </div>
