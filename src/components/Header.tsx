f113c14373f5e5cd9e048ee2b6095bcd1034cfaa
LeightonGuang <leighton.guang@icloud.com>
2024-08-03 08:49:19 +0100

    feat: add styling to login button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@
 import { logout } from "@/app/logout/actions";
 import getUser from "@/utils/getUser";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { FC, useEffect, useState } from "react";
 
 const Header = () => {
@@ -18,16 +19,23 @@ const Header = () => {
     </Link>
   );
 
-  const GuestLinks: FC = () => <NavLink href="/login">Log in</NavLink>;
+  const GuestLinks: FC = () => (
+    <NavLink href="/login">
+      <span className="mr-[1rem] rounded-[.325rem] bg-blue px-[1rem] py-[0.5rem] font-[0.875rem] text-[white] hover:bg-[#426bc2]">
+        Log in
+      </span>
+    </NavLink>
+  );
 
   const UserLinks: FC = () => (
     <div className="group relative inline-block">
-      <button className="hover:underline">Account</button>
+      <button className="hover:underline">User</button>
       <div
-        className="absolute right-0 top-[2rem] z-10 hidden flex-col group-focus-within:block"
+        className="absolute right-[-1rem] top-[2rem] z-10 hidden flex-col rounded-[.325rem] bg-gray-500 group-focus-within:block"
         id="dropdown-card"
       >
-        <div className="flex flex-col gap-[1rem]">
+        <div className="flex flex-col gap-[1rem] p-[1rem]">
+          <NavLink href="/account">Account</NavLink>
           <NavLink href="/setting">Setting</NavLink>
           <form action={logout}>
             <button className="hover:underline" type="submit">
