83650613cea3827a5ed581b9e478f4a8e611a35c
LeightonGuang <leighton.guang@icloud.com>
2024-08-08 06:35:33 +0100

    feat: context for user info

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface UserContextType {
+  user: any;
+  isLoading: boolean;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchUser = async () => {
+      const supabase = createClient();
+      const { data, error } = await supabase.auth.getUser();
+
+      if (error || !data?.user) {
+        setUser(null);
+      } else {
+        setUser(data.user);
+      }
+      setIsLoading(false);
+    };
+    fetchUser();
+  }, []);
+  return (
+    <UserContext.Provider value={{ user, isLoading }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
