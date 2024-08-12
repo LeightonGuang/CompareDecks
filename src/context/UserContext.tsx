a776c163f78442875525c9deba24a75ce286e860
LeightonGuang <leighton.guang@icloud.com>
2024-08-12 20:17:17 +0100

    feat: create fetchUserData context function

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { createClient } from "@/utils/supabase/client";
+import { clear } from "console";
 import { createContext, useContext, useEffect, useState } from "react";
 
 interface UserContextType {
@@ -8,6 +9,7 @@ interface UserContextType {
   isLoading: boolean;
   setUser: (user: any) => void;
   setIsLoading: (isLoading: boolean) => void;
+  fetchUserData: () => void;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
@@ -16,23 +18,32 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<any>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    setIsLoading(true);
-    const fetchUser = async () => {
-      const supabase = createClient();
-      const { data, error } = await supabase.auth.getUser();
+  const fetchUserData = async () => {
+    console.log("Fetching user...");
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.getUser();
 
-      if (error || !data?.user) {
-        setUser(null);
-      } else {
-        setUser(data.user);
-      }
-      setIsLoading(false);
-    };
-    fetchUser();
+    if (!data?.user) {
+      console.log("No user found");
+      setUser(null);
+    } else if (error) {
+      console.error("Failed to fetch user:", error);
+      setUser(null);
+    } else {
+      console.log("User found");
+      setUser(data.user);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUserData();
   }, []);
+
   return (
-    <UserContext.Provider value={{ user, isLoading, setUser, setIsLoading }}>
+    <UserContext.Provider
+      value={{ user, isLoading, setUser, setIsLoading, fetchUserData }}
+    >
       {children}
     </UserContext.Provider>
   );
