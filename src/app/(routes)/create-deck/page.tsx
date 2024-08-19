fde2e39712906ee8a43b867df0cc8ccdbdc5a2a7
LeightonGuang <leighton.guang@icloud.com>
2024-08-20 00:54:06 +0100

    feat: redirect to login page if user is not authenticated

--- src/app/(routes)/create-deck/page.tsx
+++ src/app/(routes)/create-deck/page.tsx
@@ -2,11 +2,19 @@
 
 import { useState } from "react";
 import CompareList from "@/components/compareList/CompareList";
+import { useRouter } from "next/navigation";
 
 import { DeckType } from "@/_types/DeckType";
+import { useUser } from "@/context/UserContext";
 
 const CreateDeckPage = () => {
+  const router = useRouter();
+  const { user } = useUser();
   const [deckData, setDeckData] = useState<DeckType | null>(null);
+
+  if (user?.aud !== "authenticated") {
+    router.push("/login");
+  }
 
   return (
     <main className="h-dynamic-vh overflow-y-auto" id="create-deck-page">
