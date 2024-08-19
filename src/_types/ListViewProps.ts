2b896b0de1ef136d99f959ffbee8d4f4cfd43463
LeightonGuang <leighton.guang@icloud.com>
2024-08-19 21:45:53 +0100

    feat: props for list view components

---
+++ src/_types/ListViewProps.ts
@@ -0,0 +1,11 @@
+import { CardType } from "./CardType";
+
+export interface ListViewProps {
+  pinnedList: CardType[];
+  unpinnedList: CardType[];
+  handlePinButton: (objIndex: number) => void;
+  handleUnpinButton: (objIndex: number) => void;
+  handleEditCardButton: () => void;
+  handleAddCardButton: () => void;
+  handleDeleteCardButton: (cardIndex: number, isPinned: boolean) => void;
+}
