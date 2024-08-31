aef38cd74b3e79261770cc3eee3fd181a4202d90
LeightonGuang <leighton.guang@icloud.com>
2024-08-31 23:20:04 +0100

    feat: new route for updateUsername

---
+++ src/app/api/updateUsername/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { updateUsername } from "@/app/updateUsername/actions";
+
+export async function POST(request: Request) {
+  try {
+    const { userId, newUsername } = await request.json();
+
+    const result = await updateUsername(userId, newUsername);
+
+    console.log(result);
+
+    if (!result?.success) {
+      return NextResponse.json(result, { status: 400 });
+    }
+
+    return NextResponse.json(
+      { message: "Username updated successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        message: `Error updating username: ${error}`,
+      },
+      { status: 500 },
+    );
+  }
+}
