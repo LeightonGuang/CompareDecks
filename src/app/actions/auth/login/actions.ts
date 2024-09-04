764fe789e970dfa8a9108731c043495d30148f26
LeightonGuang <leighton.guang@icloud.com>
2024-09-04 03:40:49 +0100

    docs: JSDoc for login function

--- src/app/actions/auth/login/actions.ts
+++ src/app/actions/auth/login/actions.ts
@@ -1,11 +1,17 @@
 "use server";
 import { createClient } from "@/utils/supabase/server";
 
+/**
+ * This function logs in the user with the given email and password using Supabase
+ *
+ * @param {string} email - The email of the user
+ * @param {string} password - The password of the user
+ * @returns {Object}
+ * @property {string} error - Only returned if there is an error
+ */
+
 export async function login(email: string, password: string) {
   const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
 
   const { error } = await supabase.auth.signInWithPassword({ email, password });
 
