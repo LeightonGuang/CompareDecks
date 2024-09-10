5d328ed3f01cd9de0be00aac8d83e1cf0b416845
LeightonGuang <leighton.guang@icloud.com>
2024-09-10 05:51:14 +0100

    feat: styling decks list

--- src/app/(routes)/my-decks/page.tsx
+++ src/app/(routes)/my-decks/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { useUser } from "@/context/UserContext";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+
+import redBinIcon from "../../../_assets/icons/redBinIcon.svg";
 
 const MyDecksPage = () => {
   const { fetchUser } = useUser();
@@ -16,8 +19,69 @@ const MyDecksPage = () => {
     <>
       {!isLoading && (
         <main className="h-dynamic-vh overflow-y-auto" id="my-deck-page">
-          <div className="mx-mobile-spacing">
-            <h1>My Decks</h1>
+          <div
+            className="mx-mobile-spacing mt-[1.5rem]"
+            id="my-deck-page__container"
+          >
+            <div
+              className="mx-auto w-min rounded-[0.5rem] border shadow-sm"
+              id="my-deck-page__card"
+            >
+              <div className="px-[1.5rem] py-[1rem]">
+                <h1 className="text-[1.5rem] font-[600] leading-[1.5rem]">
+                  My Decks
+                </h1>
+                <p className="mt-[0.375rem] text-[0.875rem] leading-[1.25rem] text-[#5E6D82]">
+                  Mange your created Decks.
+                </p>
+              </div>
+              <div className="p-[1.5rem]">
+                <table>
+                  <tr className="hover:bg-[#f9fafc]">
+                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                      Image
+                    </th>
+                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                      Name
+                    </th>
+                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                      Cards
+                    </th>
+                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                      Last Updated
+                    </th>
+                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                      Date Created
+                    </th>
+                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                      Delete
+                    </th>
+                  </tr>
+                  <tr className="border-t-[1px] border-[#E2E8F0] hover:bg-[#f9fafc]">
+                    <td className="p-[1rem]">
+                      <img
+                        className="min-h-[4rem] min-w-[4rem] object-contain"
+                        src="https://placehold.co/600x400"
+                        alt=""
+                      />
+                    </td>
+                    <td className="p-[1rem]">placeholder</td>
+                    <td className="p-[1rem]">10</td>
+                    <td className="p-[1rem]">10/09/2024</td>
+                    <td className="p-[1rem]">10/09/2024</td>
+                    <td className="p-[1rem]">
+                      <button className="mx-auto block">
+                        <Image
+                          className="h-[1.25rem] w-[1.25rem]"
+                          src={redBinIcon}
+                          alt="bin icon"
+                        />
+                      </button>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
           </div>
         </main>
       )}
