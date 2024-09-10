27a62872a4e1848e330d60d14e71eab07abb2043
LeightonGuang <leighton.guang@icloud.com>
2024-09-10 09:47:52 +0100

    feat: fetch user's deck from supabase

--- src/app/(routes)/my-decks/page.tsx
+++ src/app/(routes)/my-decks/page.tsx
@@ -3,17 +3,48 @@
 import { useUser } from "@/context/UserContext";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
 
 import redBinIcon from "../../../_assets/icons/redBinIcon.svg";
+import { DeckType } from "@/_types/DeckType";
 
 const MyDecksPage = () => {
-  const { fetchUser } = useUser();
+  const { fetchUser, user } = useUser();
   const [isLoading, setIsLoading] = useState(true);
+  const [decks, setDecks] = useState<DeckType[]>([]);
+
+  const getDeckListByUserId = async () => {
+    try {
+      console.log("userId", user);
+
+      const supabase = createClient();
+      const { data, error } = await supabase
+        .from("decks")
+        .select("*")
+        .eq("user_uid", user?.id);
+
+      if (data) {
+        setDecks(data);
+      }
+
+      if (error) {
+        console.error(error);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     fetchUser();
     setIsLoading(false);
   }, []);
+
+  useEffect(() => {
+    if (user) {
+      getDeckListByUserId();
+    }
+  }, [user]);
 
   return (
     <>
@@ -37,48 +68,75 @@ const MyDecksPage = () => {
               </div>
               <div className="p-[1.5rem]">
                 <table>
-                  <tr className="hover:bg-[#f9fafc]">
-                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
-                      Image
-                    </th>
-                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
-                      Name
-                    </th>
-                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
-                      Cards
-                    </th>
-                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
-                      Last Updated
-                    </th>
-                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
-                      Date Created
-                    </th>
-                    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
-                      Delete
-                    </th>
-                  </tr>
-                  <tr className="border-t-[1px] border-[#E2E8F0] hover:bg-[#f9fafc]">
-                    <td className="p-[1rem]">
-                      <img
-                        className="min-h-[4rem] min-w-[4rem] object-contain"
-                        src="https://placehold.co/600x400"
-                        alt=""
-                      />
-                    </td>
-                    <td className="p-[1rem]">placeholder</td>
-                    <td className="p-[1rem]">10</td>
-                    <td className="p-[1rem]">10/09/2024</td>
-                    <td className="p-[1rem]">10/09/2024</td>
-                    <td className="p-[1rem]">
-                      <button className="mx-auto block">
-                        <Image
-                          className="h-[1.25rem] w-[1.25rem]"
-                          src={redBinIcon}
-                          alt="bin icon"
-                        />
-                      </button>
-                    </td>
-                  </tr>
+                  <thead>
+                    <tr className="hover:bg-[#f9fafc]">
+                      <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                        Image
+                      </th>
+                      <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                        Name
+                      </th>
+                      <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                        Cards
+                      </th>
+                      <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                        Last Updated
+                      </th>
+                      <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                        Date Created
+                      </th>
+                      <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+                        Delete
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {decks.map((deck, i) => (
+                      <tr
+                        className="border-t-[1px] border-[#E2E8F0] hover:bg-[#f9fafc]"
+                        key={i}
+                      >
+                        <td className="p-[1rem]">
+                          <img
+                            className="min-h-[4rem] min-w-[4rem] object-contain"
+                            src="https://placehold.co/600x400"
+                            alt=""
+                          />
+                        </td>
+                        <td className="p-[1rem]">{deck.name}</td>
+                        <td className="p-[1rem]">10</td>
+                        <td className="p-[1rem]">
+                          {new Date(deck.edited_at).toLocaleDateString(
+                            "en-GB",
+                            {
+                              year: "numeric",
+                              month: "2-digit",
+                              day: "2-digit",
+                            },
+                          )}
+                        </td>
+                        <td className="p-[1rem]">
+                          {new Date(deck.created_at).toLocaleDateString(
+                            "en-GB",
+                            {
+                              year: "numeric",
+                              month: "2-digit",
+                              day: "2-digit",
+                            },
+                          )}
+                        </td>
+                        <td className="p-[1rem]">
+                          <button className="mx-auto block">
+                            <Image
+                              className="h-[1.25rem] w-[1.25rem]"
+                              src={redBinIcon}
+                              alt="bin icon"
+                            />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
                 </table>
               </div>
             </div>
