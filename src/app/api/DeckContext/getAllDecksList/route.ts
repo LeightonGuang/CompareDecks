c875dd1997ff9d8743838cc8d9d7696a0c1fa75e
LeightonGuang <leighton.guang@icloud.com>
2024-09-12 06:24:45 +0100

    feat: working api route to get a list of all decks

---
+++ src/app/api/DeckContext/getAllDecksList/route.ts
@@ -0,0 +1,25 @@
+"use server";
+import { getAllDecksList } from "@/app/actions/DeckContext/getAllDecksList/actions";
+import { NextResponse } from "next/server";
+
+export async function GET(): Promise<NextResponse> {
+  try {
+    const { allDecksList, error } = await getAllDecksList();
+
+    if (error) {
+      console.error("error from getting all deck list");
+      return NextResponse.json({ error, allDecksList: null }, { status: 500 });
+    }
+    if (!allDecksList) {
+      console.error("NO decks found");
+      return NextResponse.json(
+        { allDecksList: null, error: "No decks found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({ allDecksList }, { status: 200 });
+  } catch (error) {
+    console.error("Unexpected error: ", error);
+    return NextResponse.json({ error: "Unexpected error" }, { status: 500 });
+  }
+}
