ffe950aa3d15320a70aa7c708ebf44fbbc348734
LeightonGuang <leighton.guang@icloud.com>
2024-09-12 06:24:21 +0100

    feat: working server action to get a list of all decks

---
+++ src/app/actions/DeckContext/getAllDecksList/actions.ts
@@ -0,0 +1,36 @@
+"use server";
+import { createClient } from "@/utils/supabase/client";
+/**
+ * Get list of all decks
+ *
+ * @returns {Object}
+ * @property {DeckType[] | null} allDecksList - list of all decks
+ * @property {string} error - error message
+ */
+
+export async function getAllDecksList() {
+  try {
+    const supabase = createClient();
+    const { data, error } = await supabase
+      .from("decks")
+      .select("* , cards(imgUrl)");
+
+    if (data) {
+      return { allDecksList: data, error: null };
+    }
+
+    if (error) {
+      console.error("Error fetching decks:", error.message);
+      return { allDecksList: null, error: error.message };
+    }
+    if (!data) {
+      console.error("No decks found:", error);
+      return { allDecksList: null, error: "No decks found" };
+    }
+
+    return { allDecksList: data, error: null };
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    return { allDecksList: null, error: "Unexpected error" };
+  }
+}
