b45632f1cd2f7d2be8be9b85e15b3d611426a64c
LeightonGuang <leighton.guang@icloud.com>
2024-09-25 20:52:59 +0100

    fix: cookie error

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,51 +1,52 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const supabaseServer = createServerClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    cookies: {
-      getAll() {
-        return cookies().getAll();
-      },
-      setAll(cookiesToSet) {
-        try {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookies().set(name, value, options),
-          );
-        } catch {
-          console.error("Failed to set cookies");
-
-          // The `setAll` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
-        }
+function getSupabaseServer() {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookies().getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookies().set(name, value, options),
+            );
+          } catch {
+            console.error("Failed to set cookies");
+          }
+        },
       },
     },
-  },
-);
+  );
+}
 
-export const supabaseServerAdmin = createServerClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!,
-  {
-    cookies: {
-      getAll() {
-        return cookies().getAll();
-      },
-      setAll(cookiesToSet) {
-        try {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookies().set(name, value, options),
-          );
-        } catch {
-          console.error("Failed to set cookies");
-          // The `setAll` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
-        }
+export const supabaseServer = getSupabaseServer();
+
+function getSupabaseServerAdmin() {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookies().getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookies().set(name, value, options),
+            );
+          } catch {
+            console.error("Failed to set cookies");
+          }
+        },
       },
     },
-  },
-);
+  );
+}
+
+export const supabaseServerAdmin = getSupabaseServerAdmin();
