211ba089645de3f99a72264d818f8ec65ccec289
LeightonGuang <leighton.guang@icloud.com>
2024-09-26 23:12:54 +0100

    feat: working api route for createDeck

--- src/app/api/DeckContext/createDeck/route.ts
+++ src/app/api/DeckContext/createDeck/route.ts
@@ -1,24 +1,24 @@
 "use server";
 
-import { CardType } from "@/_types/CardType";
+import { DeckType } from "@/_types/DeckType";
 import { createDeck } from "@/app/actions/DeckContext/createDeck/actions";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
-    const {
-      supabase,
-      name,
-      user_uid,
-      cardList,
-    }: { supabase: any; name: string; user_uid: string; cardList: CardType[] } =
-      await request.json();
-    const { uuid, error } = await createDeck(supabase, name, user_uid);
+    const { deckData }: { deckData: DeckType } = await request.json();
+    const { deck_uuid, error } = await createDeck(deckData);
 
     if (error) {
-      return NextResponse.json({ data: null }, { status: 400 });
+      return NextResponse.json({ error: error }, { status: 400 });
     }
+
+    return NextResponse.json({ deck_uuid }, { status: 201 });
   } catch (error) {
-    console.error(error);
+    console.error("Error creating deck: ", error);
+    return NextResponse.json(
+      { error: "An unexpected error happened" },
+      { status: 500 },
+    );
   }
 }
