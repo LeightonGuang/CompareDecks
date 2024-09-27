f96d37311245595b03a829065630906f734c1207
LeightonGuang <leighton.guang@icloud.com>
2024-09-27 02:12:14 +0100

    fix: error with ', changing it to &#39;

--- src/app/(routes)/error/page.tsx
+++ src/app/(routes)/error/page.tsx
@@ -9,8 +9,8 @@ export default function ErrorPage() {
       <div className="mx-[1rem] text-center">
         <h1 className="text-[36px] font-[700]">Oops, something went wrong!</h1>
         <p className="mt-[1rem] text-[#5E6D82]">
-          We're sorry, but an unexpected error has occurred. Please try again
-          later or contact support if the issue persists.
+          We&#39;re sorry, but an unexpected error has occurred. Please try
+          again later or contact support if the issue persists.
         </p>
         <button
           className="mt-[1.5rem] rounded-[0.325rem] bg-blue px-[1rem] py-[0.5rem] text-[0.875rem] text-white"
