ba34b23b0bac720e9aa395e2affc1537493f7c10
LeightonGuang <leighton.guang@icloud.com>
2024-09-28 23:46:15 +0100

    fix: include header cache control no store

--- src/app/api/DeckContext/getAllDecksList/route.ts
+++ src/app/api/DeckContext/getAllDecksList/route.ts
@@ -3,26 +3,34 @@ import { getAllDecksList } from "@/app/actions/DeckContext/getAllDecksList/actio
 import { NextResponse } from "next/server";
 
 export async function GET(): Promise<NextResponse> {
+  const headers = new Headers();
+  headers.set("Cache-Control", "no-store");
+
   try {
     const { allDecksList, error } = await getAllDecksList();
 
     if (error) {
       console.error("error from getting all deck list");
-      return NextResponse.json({ error, allDecksList: null }, { status: 500 });
+      return NextResponse.json(
+        { error, allDecksList: null },
+        { status: 500, headers },
+      );
     }
+
     if (!allDecksList) {
       console.error("NO decks found");
       return NextResponse.json(
         { allDecksList: null, error: "No decks found" },
-        { status: 404 },
+        { status: 404, headers },
       );
     }
-    const response = NextResponse.json({ allDecksList }, { status: 200 });
-    response.headers.set("Cache-Control", "no-store");
 
-    return response;
+    return NextResponse.json({ allDecksList }, { status: 200, headers });
   } catch (error) {
     console.error("Unexpected error: ", error);
-    return NextResponse.json({ error: "Unexpected error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Unexpected error" },
+      { status: 500, headers },
+    );
   }
 }
