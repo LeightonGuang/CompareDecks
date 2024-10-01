87c5e3626cf26a608b87c785fb848516c505292a
LeightonGuang <leighton.guang@icloud.com>
2024-10-01 23:49:58 +0100

    feat: new getDecksByUserId server action

---
+++ src/app/actions/DeckContext/getDecksByUserId/actions.ts
@@ -0,0 +1,32 @@
+"use server";
+import { DeckType } from "@/_types/DeckType";
+import { getSupabaseServer } from "@/utils/supabase/server";
+
+/**
+ * Get decks created by user id
+ *
+ * @param userId - uuid of the user
+ * @returns {Object}
+ * @property {deckType[]} data - deck data
+ * @property {string} error - error message
+ */
+
+export async function getDecksByUserId(
+  userId: string,
+): Promise<{ decks: DeckType[] | null; error: any }> {
+  try {
+    console.log(userId);
+    const supabaseServer = getSupabaseServer();
+    const { data, error } = await supabaseServer
+      .from("decks")
+      .select("*, cards(*)")
+      .eq("user_uid", userId);
+
+    console.log("actions", data);
+
+    return { decks: data, error: error };
+  } catch (error) {
+    console.error(error);
+    return { decks: null, error: error };
+  }
+}
