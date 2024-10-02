ab8d204fb2dfbaf2f923b949ed9979b510505e20
LeightonGuang <leighton.guang@icloud.com>
2024-10-02 05:31:59 +0100

    feat: new table layout for decks

--- src/app/(routes)/decks/page.tsx
+++ src/app/(routes)/decks/page.tsx
@@ -11,7 +11,7 @@ import { useUser } from "@/context/UserContext";
 import { useDeck } from "@/context/DeckContext";
 
 const DecksPage = () => {
-  const { user, fetchUser } = useUser();
+  const { fetchUser } = useUser();
   const { getAllDecks } = useDeck();
   const [decksList, setDecksList] = useState<DeckType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -19,8 +19,7 @@ const DecksPage = () => {
   const getDecks = async () => {
     try {
       const response = await getAllDecks();
-      console.table(response);
-
+      // console.table(response);
       if (response) {
         if (response.length !== 0) {
           setDecksList(response);
@@ -50,78 +49,124 @@ const DecksPage = () => {
     fetchData();
   }, []);
 
+  const THead = ({ children }: { children: React.ReactNode }) => {
+    return (
+      <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+        {children}
+      </th>
+    );
+  };
+
+  const TData = ({
+    children,
+    className = "",
+    href = "/",
+  }: {
+    children?: React.ReactNode;
+    className?: string;
+    href?: string;
+  }) => {
+    return (
+      <td>
+        <Link className={`${className} w-min`} href={href}>
+          {children}
+        </Link>
+      </td>
+    );
+  };
+
   return (
-    <main className="h-dynamic-vh">
-      <div className="flex justify-center">
-        <div className="mx-mobile-spacing max-w-[50rem] overflow-x-auto">
-          <h1 className="font[700] text-[1.5rem]">Browse decks</h1>
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : (
-            <table className="mt-mobile-spacing w-full table-auto">
-              <thead>
-                <tr className="bg-[#f1f5f9] text-left">
-                  <th className="px-[1rem] py-[0.75rem]">Preview</th>
-                  <th className="px-[1rem] py-[0.75rem]">Deck name</th>
-                  <th className="px-[1rem] py-[0.75rem]">User id</th>
-                  <th className="px-[1rem] py-[0.75rem]">Date created</th>
-                </tr>
-              </thead>
-              <tbody>
-                {decksList &&
-                  decksList.map((deck: DeckType) => (
-                    <tr
-                      className="h-[2.75rem] border-b text-left"
-                      key={deck.id}
-                    >
-                      {deck.cards[0] && deck.cards[0] ? (
-                        <td
-                          className="flex items-center justify-center"
-                          id="preview-img"
-                        >
-                          <img
-                            className="h-[2.75rem]"
-                            src={deck.cards[0].imgUrl}
-                            alt="preview"
-                          />
-                        </td>
-                      ) : (
-                        <td
-                          className="flex items-center justify-center"
-                          id="preview-placeholder"
-                        >
-                          <Image
-                            className="aspect-[16/9] h-[2.75rem] object-cover"
-                            src={placeholder}
-                            alt="placeholder"
-                            height={49}
-                            width={49}
-                          />
-                        </td>
-                      )}
-                      <td className="h-min">
-                        <Link
-                          className="whitespace-nowrap px-[1rem] py-[0.75rem] underline"
+    <main className="h-dynamic-vh overflow-y-auto" id="decks-page">
+      <div
+        className="mx-mobile-spacing mt-[1.5rem] flex justify-center"
+        id="decks-page__container"
+      >
+        <div
+          className="w-max min-w-[40rem] rounded-[0.5rem] border shadow-sm"
+          id="decks-card"
+        >
+          <div className="px-[1.5rem] py-[1rem]">
+            <h1 className="text-[1.5rem] font-[600] leading-[1.5rem]">
+              Browse decks
+            </h1>
+            <p className="mt-[0.375rem] text-[0.875rem] leading-[1.25rem] text-[#5E6D82]">
+              Browse all the decks people have created.
+            </p>
+          </div>
+          <div>
+            <div className="p-[1.5rem]">
+              <table className="mt-mobile-spacing w-full table-auto">
+                <thead>
+                  <tr className="hover:bg-[#f9fafc]">
+                    <THead>Preview</THead>
+                    <THead>Deck name</THead>
+                    <THead>User id</THead>
+                    <THead>Date created</THead>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {isLoading ? (
+                    <tr className="p-[1rem]">
+                      <td className="p-[1rem] text-center">Loading...</td>
+                    </tr>
+                  ) : (
+                    decksList.map((deck: DeckType) => (
+                      <tr
+                        className="border-t-[1px] border-[#E2E8F0] hover:bg-[#f9fafc]"
+                        key={deck.uuid}
+                      >
+                        {deck.cards[0] && deck.cards[0] ? (
+                          <TData href={`/decks/${deck.uuid}`}>
+                            <div className="p-[1rem]">
+                              <img
+                                className="h-[4rem] w-[4rem] object-contain"
+                                src={deck.cards[0].imgUrl}
+                                alt="preview"
+                              />
+                            </div>
+                          </TData>
+                        ) : (
+                          <TData href={`/decks/${deck.uuid}`}>
+                            <div className="p-[1rem]">
+                              <Image
+                                className="h-[4rem] w-[4rem] object-contain"
+                                src={placeholder}
+                                alt="placeholder"
+                              />
+                            </div>
+                          </TData>
+                        )}
+                        <TData
+                          className="h-min whitespace-nowrap p-[1rem]"
                           href={`/decks/${deck.uuid}`}
                         >
                           {deck.name}
-                        </Link>
-                      </td>
-                      <td className="h-max-[2.75rem] whitespace-nowrap px-[1rem] py-[0.75rem]">
-                        {deck.user_uid}
-                      </td>
-                      <td className="h-max-[2.75rem] whitespace-nowrap px-[1rem] py-[0.75rem]">
-                        {new Date(deck.created_at).toLocaleDateString("en-GB", {
-                          year: "numeric",
-                          month: "2-digit",
-                          day: "2-digit",
-                        })}
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          )}
+                        </TData>
+
+                        <TData
+                          className="h-max-[2.75rem] whitespace-nowrap px-[1rem] py-[0.75rem]"
+                          href={`/decks/${deck.uuid}`}
+                        >
+                          {deck.user_uid}
+                        </TData>
+                        <td className="h-max-[2.75rem] whitespace-nowrap px-[1rem] py-[0.75rem]">
+                          {new Date(deck.created_at).toLocaleDateString(
+                            "en-GB",
+                            {
+                              year: "numeric",
+                              month: "2-digit",
+                              day: "2-digit",
+                            },
+                          )}
+                        </td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </main>
