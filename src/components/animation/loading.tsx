99790b9a68c1a8f13c81f420bcd28b421366b8d0
LeightonGuang <leighton.guang@icloud.com>
2024-10-03 02:41:52 +0100

    feat: loading animation

---
+++ src/components/animation/loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => (
+  <div className="flex w-min animate-pulse items-center justify-center space-x-2">
+    <div className="h-4 w-4 rounded-full bg-gray-400" />
+    <div className="h-4 w-4 rounded-full bg-gray-400" />
+    <div className="h-4 w-4 rounded-full bg-gray-400" />
+  </div>
+);
