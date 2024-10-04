c070a86354123652505d6fdac6db0f6a95565fbd
LeightonGuang <leighton.guang@icloud.com>
2024-10-04 01:15:17 +0100

    feat: deck page pre loading animations

--- src/app/(routes)/decks/[deckId]/page.tsx
+++ src/app/(routes)/decks/[deckId]/page.tsx
@@ -4,9 +4,9 @@ import { useEffect, useState } from "react";
 import CompareList from "@/components/compareList/CompareList";
 import { useDeck } from "@/context/DeckContext";
 import { useUser } from "@/context/UserContext";
+import { TextLoadingAnimation } from "@/components/animation/TextLoadingAnimation";
 
 import { DeckType } from "@/_types/DeckType";
-import { get } from "http";
 
 const DeckPage = ({ params }: { params: { deckId: string } }) => {
   const { user, fetchUser } = useUser();
@@ -61,7 +61,55 @@ const DeckPage = ({ params }: { params: { deckId: string } }) => {
     <main className="h-dynamic-vh overflow-y-auto" id="create-deck-page">
       <div className="mx-mobile-spacing">
         {isLoading ? (
-          <p>Loading...</p>
+          <div className="mt-[1rem] w-full" id="deck-page-loading-card">
+            <h1 className="h-[2rem] w-[16rem]">
+              <TextLoadingAnimation />
+            </h1>
+            <div className="mt-[1rem] flex h-[34rem] rounded-[0.25rem] bg-[#e0e0e0] p-[1.5rem]">
+              <ul className="w-[6.5rem]">
+                <li className="h-[2.5rem]" />
+                <li className="h-[8rem] border-b border-b-[#c5c5c5] p-[1rem]" />
+                {Array(5)
+                  .fill(0)
+                  .map((_, index) => (
+                    <li
+                      className="border-b border-b-[#c5c5c5] p-[1rem]"
+                      key={index}
+                    >
+                      <div className="h-[1.3125rem] w-full">
+                        <TextLoadingAnimation />
+                      </div>
+                    </li>
+                  ))}
+              </ul>
+              <ul className="flex">
+                {Array(5)
+                  .fill(0)
+                  .map((_, index) => (
+                    <li className="w-[10rem]" key={index}>
+                      <ul className="text-center">
+                        <li className="h-[2.5rem]" />
+                        <li className="h-[8rem] border-b border-b-[#c5c5c5] p-[0.5rem]">
+                          <TextLoadingAnimation />
+                        </li>
+                        {Array(5)
+                          .fill(0)
+                          .map((_, j) => (
+                            <li
+                              className="border-b border-b-[#c5c5c5] p-[1rem]"
+                              key={j}
+                            >
+                              <div className="h-[1.3125rem] w-full">
+                                <TextLoadingAnimation />
+                              </div>
+                            </li>
+                          ))}
+                      </ul>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          </div>
         ) : (
           <>
             <CompareList />
