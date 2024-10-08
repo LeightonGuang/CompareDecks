506737c6bda92109f23dc0a4202da4b8394e8a0f
LeightonGuang <leighton.guang@icloud.com>
2024-10-08 22:17:35 +0100

    Refactor: clean up types and useState

--- src/components/account/UpdateUsernameModal.tsx
+++ src/components/account/UpdateUsernameModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { ChangeEvent, useState } from "react";
 import { useUser } from "@/context/UserContext";
-import React from "react";
 
 interface Props {
   setShowUpdateUsernameModal: (arg0: boolean) => void;
@@ -9,9 +9,9 @@ interface Props {
 
 const UpdateUsernameModal = ({ setShowUpdateUsernameModal }: Props) => {
   const { updateUsername } = useUser();
-  const [username, setUsername] = React.useState<string>("");
+  const [username, setUsername] = useState<string>("");
 
-  const onUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value);
   };
 
@@ -35,38 +35,36 @@ const UpdateUsernameModal = ({ setShowUpdateUsernameModal }: Props) => {
         className="w-[16rem] rounded-[0.375rem] bg-white md:w-[28rem]"
         id="change-username-card"
       >
-        <div>
-          <div className="m-[1rem]">
-            <div className="mb-[1rem] flex items-center justify-between">
-              <h1>Update Username</h1>
-              <button
-                className="rounded-[0.25rem] bg-red-300 px-[0.5rem] py-[0.25rem] text-[0.75rem] text-red-500"
-                onClick={() => setShowUpdateUsernameModal(false)}
-              >
-                Close
-              </button>
-            </div>
-            <form
-              className="flex flex-col gap-[1rem]"
-              onSubmit={handleSubmitUsernameChange}
+        <div className="m-[1rem]">
+          <div className="mb-[1rem] flex items-center justify-between">
+            <h1>Update Username</h1>
+            <button
+              className="rounded-[0.25rem] bg-red-300 px-[0.5rem] py-[0.25rem] text-[0.75rem] text-red-500"
+              onClick={() => setShowUpdateUsernameModal(false)}
             >
-              <input
-                className="w-full rounded-[0.375rem] border border-[#E4E4EB] p-[0.5rem] leading-[1.375rem]"
-                name="newUsername"
-                type="text"
-                placeholder="New Username"
-                value={username}
-                onChange={onUsernameChange}
-              />
-
-              <button
-                className="rounded-[0.375rem] bg-green-500 px-[1rem] py-[0.5rem] text-white"
-                type="submit"
-              >
-                Submit
-              </button>
-            </form>
+              Close
+            </button>
           </div>
+          <form
+            className="flex flex-col gap-[1rem]"
+            onSubmit={handleSubmitUsernameChange}
+          >
+            <input
+              className="w-full rounded-[0.375rem] border border-[#E4E4EB] p-[0.5rem] leading-[1.375rem]"
+              name="newUsername"
+              type="text"
+              placeholder="New Username"
+              value={username}
+              onChange={onUsernameChange}
+            />
+
+            <button
+              className="rounded-[0.375rem] bg-green-500 px-[1rem] py-[0.5rem] text-white"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </div>
