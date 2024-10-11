ab4612fe11b8134f6866294a03e7402fa5364b27
LeightonGuang <leighton.guang@icloud.com>
2024-10-11 11:29:41 +0100

    Feat: update CardType to include attribute_values

--- src/_types/CardType.ts
+++ src/_types/CardType.ts
@@ -1,3 +1,5 @@
+import { AttributeValueType } from "./AttributeValueType";
+
 export interface CardType {
   id?: number;
   deck_uuid: string;
@@ -7,6 +9,7 @@ export interface CardType {
   year: number;
   price: string;
   description: string;
+  attribute_values: AttributeValueType[];
   created_at?: string;
   edited_at?: string;
 }
