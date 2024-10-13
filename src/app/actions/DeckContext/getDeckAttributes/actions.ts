63c968a9ca544466365128d1279c50b52e943d61
LeightonGuang <leighton.guang@icloud.com>
2024-10-13 18:11:00 +0100

    Feat: New get deck attributes supabase action

---
+++ src/app/actions/DeckContext/getDeckAttributes/actions.ts
@@ -0,0 +1,27 @@
+"use server";
+import { getSupabaseServer } from "@/utils/supabase/server";
+
+export async function getDeckAttributes(deck_uuid: string) {
+  try {
+    const supabaseServer = getSupabaseServer();
+    const { data, error } = await supabaseServer
+      .from("deck_attributes")
+      .select("*")
+      .eq("deck_uuid", deck_uuid);
+
+    if (error) {
+      console.error("Error fetching deck attributes:", error.message);
+      return { deckAttributes: null, error: error.message };
+    }
+
+    if (!data) {
+      console.error("No deck attributes found:", error);
+      return { deckAttributes: null, error: "No deck attributes found" };
+    }
+
+    return { deckAttributes: data, error: null };
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    return { deckAttributes: null, error: "Unexpected error" };
+  }
+}
