6ab05456a5f4b1c40c5e0065d1c544877a462a35
LeightonGuang <leighton.guang@icloud.com>
2024-10-15 04:34:15 +0100

    Fix: Rename type and add value

--- src/_types/AttributeValuesType.ts
+++ src/_types/AttributeValuesType.ts
@@ -1,10 +1,11 @@
 import { DeckAttributesType } from "./DeckAttributesType";
 
-export interface AttributeValueType {
+export interface AttributeValuesType {
   id: number;
+  value: string;
   attribute_id: number;
   card_id: number;
-  deck_attributes: DeckAttributesType[];
+  deck_attributes: DeckAttributesType;
   created_at: string;
   edited_at: string;
 }
