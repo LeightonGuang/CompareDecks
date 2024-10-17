bd1fa16c49d7af179c0e2fee6efaa55907b9e9de
LeightonGuang <leighton.guang@icloud.com>
2024-10-17 13:44:10 +0100

    Feat: Add custom className to THead and TData

--- src/components/list/ListComponents.tsx
+++ src/components/list/ListComponents.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
 
-export const THead = ({ children }: { children: React.ReactNode }) => {
+export const THead = ({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <th className="h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82]">
+    <th
+      className={`h-[3rem] px-[1rem] py-[1px] text-[0.875rem] font-[500] text-[#5E6D82] ${className}`}
+    >
       {children}
     </th>
   );
@@ -10,6 +18,7 @@ export const THead = ({ children }: { children: React.ReactNode }) => {
 
 export const TData = ({
   children,
+  className = "",
   href = "/",
 }: {
   children?: React.ReactNode;
@@ -17,7 +26,7 @@ export const TData = ({
   href?: string;
 }) => {
   return (
-    <td id="td">
+    <td className={className} id="td">
       <Link className="h-full" href={href} id="link">
         <div className="flex items-center justify-center p-[1rem]">
           <div className="flex h-[4rem] items-center">{children}</div>
