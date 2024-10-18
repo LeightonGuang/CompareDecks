89c9318d775f32b03baab99cbacfd43170289b5d
LeightonGuang <leighton.guang@icloud.com>
2024-10-18 18:06:49 +0100

    Feat: Update type to new database schema

--- src/_types/CardType.ts
+++ src/_types/CardType.ts
@@ -1,15 +1,11 @@
-import { AttributeValueType } from "./AttributeValueType";
+import { AttributeValuesType } from "./AttributeValuesType";
 
 export interface CardType {
-  id?: number;
+  id: number;
   deck_uuid: string;
   imgUrl: string;
-  brand: string;
-  name: string;
-  year: number;
-  price: string;
   description: string;
-  attribute_values: AttributeValueType[];
-  created_at?: string;
+  attribute_values: AttributeValuesType[];
+  created_at: string;
   edited_at?: string;
 }
