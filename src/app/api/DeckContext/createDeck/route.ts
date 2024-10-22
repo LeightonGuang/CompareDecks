11320b31ea7fbfc2f26ad4d429442d6aa3cecd20
LeightonGuang <leighton.guang@icloud.com>
2024-10-22 23:38:21 +0100

    Feat: Create deck to take deck data and attributes in api route

--- src/app/api/DeckContext/createDeck/route.ts
+++ src/app/api/DeckContext/createDeck/route.ts
@@ -1,13 +1,18 @@
 "use server";
 
+import { DeckAttributesType } from "@/_types/DeckAttributesType";
 import { DeckType } from "@/_types/DeckType";
 import { createDeck } from "@/app/actions/DeckContext/createDeck/actions";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
-    const { deckData }: { deckData: DeckType } = await request.json();
-    const { deck_uuid, error } = await createDeck(deckData);
+    const {
+      deckData,
+      attributes,
+    }: { deckData: DeckType; attributes: DeckAttributesType } =
+      await request.json();
+    const { deck_uuid, error } = await createDeck(deckData, attributes);
 
     if (error) {
       return NextResponse.json({ error: error }, { status: 400 });
