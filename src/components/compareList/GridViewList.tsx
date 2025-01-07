a12f7d40691da7a7789b936149ad6b805ace2dad
LeightonGuang <leighton.guang@icloud.com>
2025-01-07 22:25:29 +0000

    Feat: Get deckData through props

--- src/components/compareList/GridViewList.tsx
+++ src/components/compareList/GridViewList.tsx
@@ -1,42 +1,125 @@
+"use client";
+
 import Image from "next/image";
 
-import pinnedIcon from "../../_assets/icons/pinnedIcon.svg";
 import unpinIcon from "../../_assets/icons/unpinIcon.svg";
-import editIcon from "../../_assets/icons/editIcon.svg";
-import binIcon from "../../_assets/icons/binIcon.svg";
-import { useDeck } from "@/context/DeckContext";
+import pinnedIcon from "../../_assets/icons/pinnedIcon.svg";
 
 import { CardTableType } from "@/_types/CardsTableType";
+import { DecksTableType } from "@/_types/DecksTableType";
 import { AttributeValuesTableType } from "@/_types/AttributeValuesTableType";
-import { DeckAttributesTableType } from "@/_types/DeckAttributesTableType";
-import { useEffect } from "react";
 
 interface Props {
-  attributeNames: DeckAttributesTableType[];
-  handlePinButton: any;
-  handleUnpinButton: any;
-  handleEditCardButton: any;
-  handleAddCardButton: any;
-  handleDeleteCardButton: any;
-  isAuth: boolean;
+  deckData: DecksTableType | undefined;
+  pinnedList: CardTableType[];
+  handlePinButton: (cardId: number) => void;
+  unpinnedList: CardTableType[];
+  handleUnpinButton: (cardId: number) => void;
 }
 
 const GridViewList = ({
-  attributeNames,
+  deckData,
+  pinnedList,
   handlePinButton,
+  unpinnedList,
   handleUnpinButton,
-  handleEditCardButton,
-  handleAddCardButton,
-  handleDeleteCardButton,
-  isAuth,
 }: Props) => {
-  const { pinnedList, unpinnedList } = useDeck();
+  const Card = ({
+    card,
+    isPinned,
+    cardIndex,
+  }: {
+    card: CardTableType;
+    isPinned: boolean;
+    cardIndex: number;
+  }) => {
+    return (
+      <li
+        className="w-1/3 flex-shrink-0 snap-start rounded-[0.25rem] hover:bg-gray-300 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-[12.5%]"
+        id="pinned-cards"
+      >
+        <button
+          className="flex w-full justify-center"
+          onClick={() =>
+            isPinned ? handleUnpinButton(card.id) : handlePinButton(card.id)
+          }
+          id="unpin-button"
+        >
+          <Image
+            className="m-[0.625rem] min-h-[1.25rem] min-w-[1.25rem]"
+            src={isPinned ? pinnedIcon : unpinIcon}
+            alt="pinned icon"
+            height={20}
+            width={20}
+          />
+        </button>
+        <div
+          className="flex h-[4rem] w-full items-center justify-center border-b border-b-[#c5c5c5] bg-black md:h-[6rem] xl:h-[8rem]"
+          id="image-container"
+        >
+          <img
+            alt={"card image"}
+            className="w-full object-contain"
+            src={card.imgUrl}
+          />
+        </div>
+        {card.attribute_values.map(
+          (
+            _attributeObj: AttributeValuesTableType,
+            attributeObjIndex: number,
+          ) => (
+            <div
+              className="min-h-[3.375rem] overflow-x-auto whitespace-nowrap border-b border-b-[#c5c5c5] px-[0.5rem] py-[1rem] text-center text-[0.875rem] font-[400]"
+              key={attributeObjIndex}
+            >
+              {card.attribute_values[attributeObjIndex].value}
+            </div>
+          ),
+        )}
+        <div
+          className="h-[10rem] overflow-y-auto border-b border-b-[#c5c5c5] px-[0.5rem] py-[1rem] text-[0.875rem] font-[400]"
+          id="row"
+        >
+          <p className="leading-tight">{card.description}</p>
+        </div>
+      </li>
+    );
+  };
 
-  useEffect(() => {
-    console.log("attributeNames changed: ", attributeNames);
-  }, [attributeNames]);
+  const PinnedCards = ({ pinnedCards }: { pinnedCards: CardTableType[] }) => {
+    const havePinnedCards = pinnedList.length > 0;
 
-  return attributeNames.length > 0 ? (
+    return (
+      havePinnedCards &&
+      pinnedCards.map((pinnedCard: CardTableType, cardIndex: number) => (
+        <Card key={cardIndex} card={pinnedCard} isPinned={true} cardIndex={cardIndex} />
+      ))
+    );
+  };
+
+  const UnpinnedCards = ({
+    unpinnedCards,
+  }: {
+    unpinnedCards: CardTableType[];
+  }) => {
+    const haveUnpinnedCards = unpinnedList.length > 0;
+
+    return (
+      haveUnpinnedCards &&
+      unpinnedCards.map((unpinnedCard: CardTableType, cardIndex: number) => {
+        return (
+          <Card
+            key={cardIndex}
+            card={unpinnedCard}
+            isPinned={false}
+            cardIndex={cardIndex}
+          />
+        );
+      })
+    );
+  };
+
+  return (
     <div
       className="mt-[1rem] flex rounded-[0.25rem] bg-[#e0e0e0] p-[1.5rem]"
       id="grid-view-list"
@@ -49,7 +132,7 @@ const GridViewList = ({
             id="image-spacer"
           />
         </li>
-        {attributeNames.map((attributeObj: any, index: number) => (
+        {deckData?.deck_attributes.map((attributeObj: any, index: number) => (
           <li
             className="border-b border-b-[#c5c5c5] p-[1rem] text-[0.875rem]"
             key={index}
@@ -63,191 +146,10 @@ const GridViewList = ({
         className="scrollbar-top flex min-h-[15rem] w-full snap-x snap-mandatory list-none flex-row overflow-x-auto scroll-smooth pb-[1rem]"
         id="scroll-list"
       >
-        {pinnedList.length > 0 &&
-          pinnedList.map((pinnedCardObj: CardTableType, cardIndex: number) => (
-            <li
-              className="w-1/3 flex-shrink-0 snap-start rounded-[0.25rem] hover:bg-gray-300 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-[12.5%]"
-              key={cardIndex}
-              id="pinned-cards"
-            >
-              <button
-                className="flex w-full justify-center"
-                onClick={() => handleUnpinButton(cardIndex)}
-                id="unpin-button"
-              >
-                <Image
-                  className="m-[0.625rem] min-h-[1.25rem] min-w-[1.25rem]"
-                  src={pinnedIcon}
-                  alt="pinned icon"
-                  height={20}
-                  width={20}
-                />
-              </button>
-              <div
-                className="flex h-[4rem] w-full items-center justify-center border-b border-b-[#c5c5c5] bg-black md:h-[6rem] xl:h-[8rem]"
-                id="image-container"
-              >
-                <img
-                  className="w-full object-contain"
-                  src={pinnedCardObj.imgUrl}
-                  alt=""
-                />
-              </div>
-              {pinnedCardObj.attribute_values.map(
-                (
-                  _attributeObj: AttributeValuesTableType,
-                  attributeObjIndex: number,
-                ) => (
-                  <div
-                    className="min-h-[3.375rem] overflow-x-auto whitespace-nowrap border-b border-b-[#c5c5c5] px-[0.5rem] py-[1rem] text-center text-[0.875rem] font-[400]"
-                    key={attributeObjIndex}
-                  >
-                    {
-                      pinnedCardObj.attribute_values.find(
-                        (attributeValue: AttributeValuesTableType) =>
-                          attributeValue.deck_attributes.attribute ===
-                          (
-                            attributeNames[
-                              attributeObjIndex
-                            ] as DeckAttributesTableType
-                          ).attribute,
-                      )?.value
-                    }
-                  </div>
-                ),
-              )}
-              <div
-                className="h-[10rem] overflow-y-auto border-b border-b-[#c5c5c5] px-[0.5rem] py-[1rem] text-center text-[0.875rem] font-[400]"
-                id="row"
-              >
-                {pinnedCardObj.description}
-              </div>
-              {isAuth && (
-                <div className="flex justify-center gap-[1rem] p-[1rem] sm:gap-[2rem]">
-                  <button onClick={() => handleEditCardButton(cardIndex, true)}>
-                    <Image
-                      src={editIcon}
-                      alt="edit icon"
-                      height={20}
-                      width={20}
-                    />
-                  </button>
-                  <button
-                    onClick={() => handleDeleteCardButton(cardIndex, true)}
-                  >
-                    <Image
-                      src={binIcon}
-                      alt="delete icon"
-                      height={20}
-                      width={20}
-                    />
-                  </button>
-                </div>
-              )}
-            </li>
-          ))}
-        {unpinnedList.length > 0 &&
-          unpinnedList.map(
-            (unpinnedCardObj: CardTableType, cardIndex: number) => {
-              return (
-                <li
-                  className="w-1/3 flex-shrink-0 snap-start rounded-[0.25rem] hover:bg-gray-300 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-[12.5%]"
-                  key={cardIndex}
-                  id="unpinned-card"
-                >
-                  <button
-                    className="flex w-full justify-center"
-                    onClick={() => handlePinButton(cardIndex)}
-                  >
-                    <Image
-                      className="m-[0.625rem]"
-                      src={unpinIcon}
-                      alt="pinned icon"
-                      height={20}
-                      width={20}
-                    />
-                  </button>
-                  <div
-                    className="flex h-[4rem] w-full items-center justify-center border-b border-b-[#c5c5c5] bg-black md:h-[6rem] xl:h-[8rem]"
-                    id="image-container"
-                  >
-                    <img
-                      className="h-min w-full object-contain"
-                      src={unpinnedCardObj.imgUrl}
-                      alt=""
-                    />
-                  </div>
-                  {unpinnedCardObj.attribute_values.map(
-                    (
-                      _attributeObj: AttributeValuesTableType,
-                      attributeObjIndex: number,
-                    ) => (
-                      <div
-                        className="min-h-[3.375rem] overflow-x-auto whitespace-nowrap border-b border-b-[#c5c5c5] px-[0.5rem] py-[1rem] text-center text-[0.875rem] font-[400]"
-                        key={attributeObjIndex}
-                      >
-                        {
-                          unpinnedCardObj.attribute_values.find(
-                            (attributeValue: AttributeValuesTableType) =>
-                              attributeValue.deck_attributes.attribute ===
-                              (
-                                attributeNames[
-                                  attributeObjIndex
-                                ] as DeckAttributesTableType
-                              ).attribute,
-                          )?.value
-                        }
-                      </div>
-                    ),
-                  )}
-                  <div
-                    className="h-[10rem] overflow-y-auto border-b border-b-[#c5c5c5] px-[0.5rem] py-[1rem] text-center text-[0.875rem] font-[400]"
-                    id="row"
-                  >
-                    {unpinnedCardObj.description}
-                  </div>
-                  {isAuth && (
-                    <div className="flex justify-center gap-[1rem] p-[1rem] sm:gap-[2rem]">
-                      <button
-                        onClick={() => handleEditCardButton(cardIndex, false)}
-                      >
-                        <Image
-                          src={editIcon}
-                          alt="edit icon"
-                          height={20}
-                          width={20}
-                        />
-                      </button>
-                      <button
-                        onClick={() => handleDeleteCardButton(cardIndex, false)}
-                      >
-                        <Image
-                          src={binIcon}
-                          alt="delete icon"
-                          height={20}
-                          width={20}
-                        />
-                      </button>
-                    </div>
-                  )}
-                </li>
-              );
-            },
-          )}
-        {isAuth && (
-          <li className="flex h-full w-1/3 flex-shrink-0 snap-start items-center justify-center rounded-[0.25rem] border border-[#c5c5c5] hover:bg-gray-300 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-[12.5%]">
-            <button
-              className="h-full w-full text-[4rem] font-[400]"
-              onClick={() => handleAddCardButton()}
-            >
-              <span>+</span>
-            </button>
-          </li>
-        )}
+        <PinnedCards pinnedCards={pinnedList} />
+        <UnpinnedCards unpinnedCards={unpinnedList} />
       </ul>
     </div>
-  ) : (
-    <div>no attribute name</div>
   );
 };
 
