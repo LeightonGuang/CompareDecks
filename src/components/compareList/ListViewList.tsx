129dacc9957e2db71ceded0a181ab842172315c2
LeightonGuang <leighton.guang@icloud.com>
2025-01-07 23:13:10 +0000

    Refactor: Clean up code structure

--- src/components/compareList/ListViewList.tsx
+++ src/components/compareList/ListViewList.tsx
@@ -1,180 +1,128 @@
 import Image from "next/image";
-
-import pinnedIcon from "../../_assets/icons/pinnedIcon.svg";
 import unpinIcon from "../../_assets/icons/unpinIcon.svg";
-import editIcon from "../../_assets/icons/editIcon.svg";
-import binIcon from "../../_assets/icons/binIcon.svg";
-import { useDeck } from "@/context/DeckContext";
+import pinnedIcon from "../../_assets/icons/pinnedIcon.svg";
+
+import { CardTableType } from "@/_types/CardsTableType";
+import { DecksTableType } from "@/_types/DecksTableType";
 
 interface Props {
-  handlePinButton: (objIndex: number) => void;
-  handleUnpinButton: (objIndex: number) => void;
-  handleEditCardButton: (cardIndex: number, isPinned: boolean) => void;
-  handleAddCardButton: () => void;
-  handleDeleteCardButton: (cardIndex: number, isPinned: boolean) => void;
-  isAuth: boolean;
+  deckData: DecksTableType | undefined;
+  pinnedList: CardTableType[];
+  handlePinButton: (cardId: number) => void;
+  unpinnedList: CardTableType[];
+  handleUnpinButton: (cardId: number) => void;
 }
 
 const ListViewList = ({
+  deckData,
+  pinnedList,
   handlePinButton,
+  unpinnedList,
   handleUnpinButton,
-  handleEditCardButton,
-  handleAddCardButton,
-  handleDeleteCardButton,
-  isAuth,
 }: Props) => {
-  const { pinnedList, unpinnedList } = useDeck();
+  const Row = ({
+    card,
+    isPinned,
+    cardIndex,
+  }: {
+    card: CardTableType;
+    isPinned: boolean;
+    cardIndex: number;
+  }) => {
+    return (
+      <tr className={`max-h-[3rem] ${cardIndex % 2 === 0 && `bg-[#e0e0e0]`}`}>
+        <td className="px-[1rem]">{card.order}.</td>
+
+        <td className="px-[1rem]">
+          <button
+            className="h-[1.25rem] w-[1.25rem]"
+            onClick={() =>
+              isPinned ? handleUnpinButton(card.id) : handlePinButton(card.id)
+            }
+          >
+            <Image
+              className="h-[1.25rem] w-[1.25rem]"
+              src={isPinned ? pinnedIcon : unpinIcon}
+              alt={isPinned ? "pinned icon" : "unpinned icon"}
+            />
+          </button>
+        </td>
+
+        <td className="px-[1rem]">
+          <div className="flex max-h-[4rem] w-[6rem] justify-center bg-black">
+            <img
+              className="h-[4rem] object-contain"
+              src={card.imgUrl}
+              alt={"card image"}
+              id="item-image"
+            />
+          </div>
+        </td>
+
+        {card.attribute_values.map((attribute, attributeIndex) => (
+          <td key={attributeIndex}>{attribute.value}</td>
+        ))}
+
+        <td className="whitespace-nowrap px-[1rem]">
+          <p>{card.description}</p>
+        </td>
+      </tr>
+    );
+  };
+
+  const PinnedRows = ({ pinnedrows }: { pinnedrows: CardTableType[] }) => {
+    const havePinnedRows = pinnedList.length > 0;
+
+    return (
+      havePinnedRows &&
+      pinnedrows.map((cardObj, cardIndex) => (
+        <Row
+          key={cardIndex}
+          card={cardObj}
+          isPinned={true}
+          cardIndex={cardIndex}
+        />
+      ))
+    );
+  };
+
+  const UnpinnedRows = ({
+    unpinnedrows,
+  }: {
+    unpinnedrows: CardTableType[];
+  }) => {
+    const haveUnpinnedRows = unpinnedrows.length > 0;
+
+    return (
+      haveUnpinnedRows &&
+      unpinnedrows.map((cardObj, cardIndex) => (
+        <Row
+          key={cardIndex}
+          card={cardObj}
+          isPinned={false}
+          cardIndex={cardIndex}
+        />
+      ))
+    );
+  };
 
   return (
-    <div className="w-full overflow-x-auto py-[1rem]" id="table-view-container">
+    <div className="w-full overflow-x-auto py-4" id="table-view-container">
       <table className="w-full">
-        <thead className="">
+        <thead>
           <tr>
             <th></th>
             <th></th>
-            <th className="px-[1rem] text-left">Image</th>
-            <th className="px-[1rem] text-left">Name</th>
-            <th className="px-[1rem] text-left">Brand</th>
-            <th className="px-[1rem] text-left">Year</th>
-            <th className="px-[1rem] text-left">Price</th>
-            <th className="px-[1rem] text-left">Description</th>
-            {isAuth && <th>action</th>}
+            <th></th>
+            {deckData?.deck_attributes.map((attribute) => {
+              return <th key={attribute.id}>{attribute.attribute}</th>;
+            })}
+            <th>description</th>
           </tr>
         </thead>
-        <tbody className="">
-          {pinnedList.map((cardObj, cardIndex) => (
-            <tr
-              className={`max-h-[3rem] ${
-                cardIndex % 2 === 0 && `bg-[#e0e0e0]`
-              }`}
-              key={cardIndex}
-            >
-              <td className="px-[1rem]">{cardIndex + 1}.</td>
-              <td className="px-[1rem]">
-                <button
-                  className="h-[1.25rem] w-[1.25rem]"
-                  onClick={() => handleUnpinButton(cardIndex)}
-                >
-                  <Image
-                    className="h-[1.25rem] w-[1.25rem]"
-                    src={pinnedIcon}
-                    alt="pinned icon"
-                  />
-                </button>
-              </td>
-              <td className="px-[1rem]">
-                <div className="flex max-h-[4rem] w-[6rem] justify-center bg-black">
-                  <img
-                    className="h-[4rem] object-contain"
-                    src={cardObj.imgUrl}
-                    alt={cardObj.brand + " " + cardObj.name}
-                    id="item-image"
-                  />
-                </div>
-              </td>
-              <td className="whitespace-nowrap px-[1rem]">{cardObj.name}</td>
-              <td className="whitespace-nowrap px-[1rem]">{cardObj.brand}</td>
-              <td className="whitespace-nowrap px-[1rem]">{cardObj.year}</td>
-              <td className="whitespace-nowrap px-[1rem]">{cardObj.price}</td>
-              <td className="whitespace-nowrap px-[1rem]">
-                <p>{cardObj.description}</p>
-              </td>
-              {isAuth && (
-                <td>
-                  <div className="flex justify-center gap-mobile-spacing px-[1rem]">
-                    <button
-                      onClick={() => handleEditCardButton(cardIndex, true)}
-                    >
-                      <Image
-                        src={editIcon}
-                        alt="edit icon"
-                        height={20}
-                        width={20}
-                      />
-                    </button>
-                    <button
-                      onClick={() => handleDeleteCardButton(cardIndex, true)}
-                    >
-                      <Image
-                        src={binIcon}
-                        alt="bin icon"
-                        height={20}
-                        width={20}
-                      />
-                    </button>
-                  </div>
-                </td>
-              )}
-            </tr>
-          ))}
-          {unpinnedList.map((cardObj, cardIndex) => (
-            <tr
-              className={`max-h-[3rem] ${
-                (pinnedList.length + cardIndex) % 2 === 0 && `bg-[#e0e0e0]`
-              }`}
-              key={cardIndex}
-            >
-              <td className="px-[1rem]">
-                {pinnedList.length + cardIndex + 1}.
-              </td>
-              <td className="px-[1rem]">
-                <button onClick={() => handlePinButton(cardIndex)}>
-                  <Image
-                    className="min-h-[1.25rem] min-w-[1.25rem]"
-                    src={unpinIcon}
-                    alt="unpin icon"
-                    width={20}
-                    height={20}
-                  />
-                </button>
-              </td>
-              <td className="px-[1rem]">
-                <div className="flex max-h-[4rem] w-[6rem] justify-center bg-black">
-                  <img
-                    className="h-[4rem] object-contain"
-                    src={cardObj.imgUrl}
-                    alt={cardObj.brand + " " + cardObj.name}
-                    id="item-image"
-                  />
-                </div>
-              </td>
-              <td className="whitespace-nowrap px-[1rem]">{cardObj.name}</td>
-              <td className="whitespace-nowrap px-[1rem]">{cardObj.brand}</td>
-              <td className="whitespace-nowrap px-[1rem]">{cardObj.year}</td>
-              <td className="whitespace-nowrap px-[1rem]">{cardObj.price}</td>
-              <td className="whitespace-nowrap px-[1rem]">
-                <p>{cardObj.description}</p>
-              </td>
-              {isAuth && (
-                <td>
-                  <div className="flex justify-center gap-mobile-spacing px-[1rem]">
-                    <button
-                      onClick={() => handleEditCardButton(cardIndex, false)}
-                    >
-                      <Image
-                        className="min-h-[1.25rem] min-w-[1.25rem]"
-                        src={editIcon}
-                        alt="edit icon"
-                        width={20}
-                        height={20}
-                      />
-                    </button>
-                    <button
-                      onClick={() => handleDeleteCardButton(cardIndex, false)}
-                    >
-                      <Image
-                        className="min-h-[1.25rem] min-w-[1.25rem]"
-                        src={binIcon}
-                        alt="bin icon"
-                        width={20}
-                        height={20}
-                      />
-                    </button>
-                  </div>
-                </td>
-              )}
-            </tr>
-          ))}
+        <tbody>
+          <PinnedRows pinnedrows={pinnedList} />
+          <UnpinnedRows unpinnedrows={unpinnedList} />
         </tbody>
       </table>
     </div>
