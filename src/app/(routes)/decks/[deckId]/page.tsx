e298b728898eefb53906635399d89eaae32cfda3
LeightonGuang <leighton.guang@icloud.com>
2025-01-08 12:43:19 +0000

    Feat: Move pinnedList and unpinnedList for compare list to handle

--- src/app/(routes)/decks/[deckId]/page.tsx
+++ src/app/(routes)/decks/[deckId]/page.tsx
@@ -2,132 +2,92 @@
 
 import { useEffect, useState } from "react";
 import CompareList from "@/components/compareList/CompareList";
-import { useDeck } from "@/context/DeckContext";
-import { useUser } from "@/context/UserContext";
+import { getDeckById } from "@/app/actions/DeckContext/getDeckById/actions";
 import { TextLoadingAnimation } from "@/components/animation/TextLoadingAnimation";
 
 import { DecksTableType } from "@/_types/DecksTableType";
 
 const DeckPage = ({ params }: { params: { deckId: string } }) => {
-  const { user, fetchUser } = useUser();
-  const {
-    getDeckById,
-    pendingDeckData,
-    setPendingDeckData,
-    originalDeckData,
-    setOriginalDeckData,
-  } = useDeck();
-
   const [isLoading, setIsLoading] = useState(true);
+  const [deckData, setDeckData] = useState<DecksTableType | undefined>();
 
-  const getDeck = async () => {
-    setPendingDeckData({} as DecksTableType);
+  const LoadingSkeleton = () => (
+    <div className="mt-[1rem] w-full" id="deck-page-loading-card">
+      <h1 className="h-[2rem] w-[16rem]">
+        <TextLoadingAnimation />
+      </h1>
+      <div className="mt-[1rem] flex h-[34rem] rounded-[0.25rem] bg-[#e0e0e0] p-[1.5rem]">
+        <ul className="w-[6.5rem]">
+          <li className="h-[2.5rem]" />
+          <li className="h-[8rem] border-b border-b-[#c5c5c5] p-[1rem]" />
+          {Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <li className="border-b border-b-[#c5c5c5] p-[1rem]" key={index}>
+                <div className="h-[1.3125rem] w-full">
+                  <TextLoadingAnimation />
+                </div>
+              </li>
+            ))}
+        </ul>
+        <ul className="flex">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <li className="w-[10rem]" key={index}>
+                <ul className="text-center">
+                  <li className="h-[2.5rem]" />
+                  <li className="h-[8rem] border-b border-b-[#c5c5c5] p-[0.5rem]">
+                    <TextLoadingAnimation />
+                  </li>
+                  {Array(5)
+                    .fill(0)
+                    .map((_, j) => (
+                      <li
+                        className="border-b border-b-[#c5c5c5] p-[1rem]"
+                        key={j}
+                      >
+                        <div className="h-[1.3125rem] w-full">
+                          <TextLoadingAnimation />
+                        </div>
+                      </li>
+                    ))}
+                </ul>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+
+  const fetchData = async () => {
     try {
-      const response = await getDeckById(params.deckId);
+      const { data: deckData, error: deckError } = await getDeckById(
+        params.deckId,
+      );
 
-      if (response) {
-        const { success, error, data } = response;
-
-        if (data !== null) {
-          setOriginalDeckData(data[0]);
-          setPendingDeckData(data[0]);
-        }
-
-        if (!success) {
-          console.error(error);
-        }
+      if (deckError) {
+        console.error(deckError);
+      } else if (deckData) {
+        setDeckData(deckData);
       }
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    setIsLoading(true);
-    const fetchData = async () => {
-      try {
-        await fetchUser();
-        await getDeck();
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
     fetchData();
   }, []);
 
   return (
-    <main className="h-dynamic-vh overflow-y-auto" id="create-deck-page">
+    <section className="h-dynamic-vh overflow-y-auto" id="create-deck-page">
       <div className="mx-mobile-spacing">
-        {isLoading ? (
-          <div className="mt-[1rem] w-full" id="deck-page-loading-card">
-            <h1 className="h-[2rem] w-[16rem]">
-              <TextLoadingAnimation />
-            </h1>
-            <div className="mt-[1rem] flex h-[34rem] rounded-[0.25rem] bg-[#e0e0e0] p-[1.5rem]">
-              <ul className="w-[6.5rem]">
-                <li className="h-[2.5rem]" />
-                <li className="h-[8rem] border-b border-b-[#c5c5c5] p-[1rem]" />
-                {Array(5)
-                  .fill(0)
-                  .map((_, index) => (
-                    <li
-                      className="border-b border-b-[#c5c5c5] p-[1rem]"
-                      key={index}
-                    >
-                      <div className="h-[1.3125rem] w-full">
-                        <TextLoadingAnimation />
-                      </div>
-                    </li>
-                  ))}
-              </ul>
-              <ul className="flex">
-                {Array(5)
-                  .fill(0)
-                  .map((_, index) => (
-                    <li className="w-[10rem]" key={index}>
-                      <ul className="text-center">
-                        <li className="h-[2.5rem]" />
-                        <li className="h-[8rem] border-b border-b-[#c5c5c5] p-[0.5rem]">
-                          <TextLoadingAnimation />
-                        </li>
-                        {Array(5)
-                          .fill(0)
-                          .map((_, j) => (
-                            <li
-                              className="border-b border-b-[#c5c5c5] p-[1rem]"
-                              key={j}
-                            >
-                              <div className="h-[1.3125rem] w-full">
-                                <TextLoadingAnimation />
-                              </div>
-                            </li>
-                          ))}
-                      </ul>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </div>
-        ) : (
-          <>
-            <CompareList />
-            <div className="absolute right-0">
-              <button
-                className="ml-[auto] mr-[1rem] mt-[1rem] rounded-[0.325rem] bg-green-500 px-[1rem] py-[0.5rem] text-white disabled:bg-gray-400 disabled:text-black"
-                disabled={
-                  JSON.stringify(pendingDeckData) ===
-                  JSON.stringify(originalDeckData)
-                }
-              >
-                Update
-              </button>
-            </div>
-          </>
-        )}
+        {isLoading ? <LoadingSkeleton /> : <CompareList deckData={deckData} />}
       </div>
-    </main>
+    </section>
   );
 };
 
--- src/components/compareList/CompareList.tsx
+++ src/components/compareList/CompareList.tsx
@@ -1,222 +1,76 @@
 "use client";
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
-import { useUser } from "@/context/UserContext";
-import AddCardModal from "./AddCardModal";
-import EditCardModal from "./EditCardModal";
 import GridViewList from "./GridViewList";
 import ListViewList from "./ListViewList";
 import ListViewButton from "./ListViewButton";
-import { useDeck } from "@/context/DeckContext";
-import editIcon from "../../_assets/icons/editIcon.svg";
 
 import { CardTableType } from "@/_types/CardsTableType";
+import { DecksTableType } from "@/_types/DecksTableType";
 
-const CompareList = () => {
-  const { user } = useUser();
-  const {
-    attributeNames,
-    setAttributeNames,
-    orderedList,
-    setOrderedList,
-    pinnedList,
-    setPinnedList,
-    unpinnedList,
-    setUnpinnedList,
-    pendingDeckData,
-    setPendingDeckData,
-  } = useDeck();
-  const isAuth = user?.aud === "authenticated";
-  const [isLoading, setIsLoading] = useState(true);
-  const [cardFormData, setCardFormData] = useState<CardTableType | null>(null);
-  const [deckName, setDeckName] = useState<string>("");
-  const [isEditDeckName, setIsEditDeckName] = useState<boolean>(false);
-  const [isListView, setIsListView] = useState<boolean>(false);
-  const [isShowAddCardModal, setIsShowAddCardModal] = useState<boolean>(false);
-  const [isShowEditCardModal, setIsShowEditCardModal] =
-    useState<boolean>(false);
+/* !Required data
+  - user info
+  - deck info
+  - cards
+  - deck attributes
+*/
 
-  const getDeckAttributes = async (deck_uuid: string) => {
-    try {
-      const response = await fetch(`/api/getDeckAttributes`, {
-        method: "POST",
-        body: JSON.stringify({
-          deck_uuid: deck_uuid,
-        }),
-      });
-      const data = await response.json();
+const CompareList = ({
+  deckData,
+}: {
+  deckData: DecksTableType | undefined;
+}) => {
+  const [isListView, setIsListView] = useState(false);
+  const [pinnedList, setPinnedList] = useState<CardTableType[]>([]);
+  const [unpinnedList, setUnpinnedList] = useState<CardTableType[]>([]);
 
-      if (data.deckAttributes) {
-        setAttributeNames(data.deckAttributes);
-      }
-    } catch (error) {
-      console.error("Error fetching deck attributes:", error);
-    }
+  const handlePinButton = (cardId: number) => {
+    const pinnedCard = unpinnedList.find((card) => card.id === cardId);
+
+    setPinnedList([...pinnedList, pinnedCard!]);
+    setUnpinnedList(unpinnedList.filter((card) => card.id !== cardId));
   };
 
-  const handlePinButton: (objIndex: number) => void = (objIndex) => {
-    setPinnedList([...pinnedList, unpinnedList[objIndex]]);
-    setUnpinnedList((prevList) =>
-      prevList.filter((_obj, index) => index !== objIndex),
-    );
-  };
+  const handleUnpinButton = (cardId: number) => {
+    const unpinnedCard = pinnedList.find((card) => card.id === cardId);
 
-  const handleUnpinButton = (objIndex: number) => {
-    const itemToUnpin = pinnedList[objIndex];
-    setPinnedList((prevList) =>
-      prevList.filter((_obj, index) => index !== objIndex),
-    );
-
-    const originalIndex = orderedList.findIndex(
-      (item) => item.id === itemToUnpin.id,
-    );
-
-    setUnpinnedList((prevList) => {
-      const newList = [...prevList];
-      newList.splice(originalIndex, 0, itemToUnpin);
-      return newList;
-    });
-  };
-
-  const handleEditDeckName = () => {
-    setIsEditDeckName(true);
-  };
-
-  const onDeckNameChange = (e: any) => {
-    setDeckName(e.target.value);
-  };
-
-  const handleDeckNameSubmit = (e: any) => {
-    e.preventDefault();
-    setPendingDeckData({ ...pendingDeckData, name: deckName });
-    setIsEditDeckName(false);
-  };
-
-  const handleAddCardButton = () => {
-    setIsShowAddCardModal(true);
-  };
-
-  const handleEditCardButton = (cardIndex: number, isPinned: boolean) => {
-    setIsShowEditCardModal(true);
-    if (isPinned) {
-      setCardFormData(pinnedList[cardIndex]);
-    } else if (!isPinned) {
-      setCardFormData(unpinnedList[cardIndex]);
-    }
-  };
-
-  const handleDeleteCardButton = (cardIndex: number, isPinned: boolean) => {
-    if (isPinned) {
-      setPinnedList((prevList) =>
-        prevList.filter((_obj, index) => index !== cardIndex),
-      );
-    } else if (!isPinned) {
-      setUnpinnedList((prevList) =>
-        prevList.filter((_obj, index) => index !== cardIndex),
-      );
-    }
+    setUnpinnedList([...unpinnedList, unpinnedCard!]);
+    setPinnedList(pinnedList.filter((card) => card.id !== cardId));
   };
 
   useEffect(() => {
-    setDeckName(pendingDeckData.name);
-    setOrderedList(pendingDeckData.cards);
-    setUnpinnedList(pendingDeckData.cards);
-    getDeckAttributes(pendingDeckData.uuid);
-    setIsLoading(false);
-  }, [pendingDeckData]);
+    if (deckData) {
+      setUnpinnedList(deckData.cards);
+      console.log(deckData.cards);
+    }
+  }, [deckData]);
 
   return (
-    <>
-      {!isLoading && (
-        <div id="compare-list">
-          <div
-            className="mt-[1rem] flex items-center justify-between"
-            id="compare-list-header"
-          >
-            <div
-              className="flex items-center gap-[0.5rem]"
-              id="compare-list-title"
-            >
-              {isEditDeckName ? (
-                <form
-                  className="flex h-[2rem] gap-[1rem]"
-                  onSubmit={handleDeckNameSubmit}
-                >
-                  <input
-                    className="w-[12rem] rounded-[0.375rem] border"
-                    onChange={onDeckNameChange}
-                    value={deckName}
-                    type="text"
-                  />
-                  <button
-                    className="rounded-[0.375rem] bg-[#2563eb] px-[0.5rem] py-[0.25rem] text-[0.75rem] text-white"
-                    type="submit"
-                  >
-                    Save
-                  </button>
-                </form>
-              ) : (
-                <>
-                  <h2 className="text-[1.5rem] font-bold leading-[2rem]">
-                    {pendingDeckData.name ? pendingDeckData.name : ""}
-                  </h2>
-                  {user && user?.id === pendingDeckData?.user_uid ? (
-                    <button
-                      className="mx-[0.625rem]"
-                      onClick={handleEditDeckName}
-                    >
-                      <Image
-                        src={editIcon}
-                        alt="edit icon"
-                        height={20}
-                        width={20}
-                      />
-                    </button>
-                  ) : (
-                    ""
-                  )}
-                </>
-              )}
-            </div>
-            <ListViewButton
-              isListView={isListView}
-              setIsListView={setIsListView}
-            />
-          </div>
-          {!isListView ? (
-            <GridViewList
-              attributeNames={attributeNames}
-              handlePinButton={handlePinButton}
-              handleUnpinButton={handleUnpinButton}
-              handleEditCardButton={handleEditCardButton}
-              handleAddCardButton={handleAddCardButton}
-              handleDeleteCardButton={handleDeleteCardButton}
-              isAuth={isAuth}
-            />
-          ) : (
-            <ListViewList
-              handlePinButton={handlePinButton}
-              handleUnpinButton={handleUnpinButton}
-              handleEditCardButton={handleEditCardButton}
-              handleAddCardButton={handleAddCardButton}
-              handleDeleteCardButton={handleDeleteCardButton}
-              isAuth={isAuth}
-            />
-          )}
-          {isShowAddCardModal && (
-            <AddCardModal setIsShowAddCardModal={setIsShowAddCardModal} />
-          )}
-          {isShowEditCardModal && (
-            <EditCardModal
-              cardFormData={cardFormData}
-              setCardFormData={setCardFormData}
-              setIsShowEditCardModal={setIsShowEditCardModal}
-            />
-          )}
-        </div>
+    <div className="mt-4 flex flex-col">
+      <div className="flex items-center justify-between">
+        <h1>{deckData?.name}</h1>
+
+        <ListViewButton isListView={isListView} setIsListView={setIsListView} />
+      </div>
+
+      {isListView ? (
+        <ListViewList
+          deckData={deckData}
+          pinnedList={pinnedList}
+          handlePinButton={handlePinButton}
+          unpinnedList={unpinnedList}
+          handleUnpinButton={handleUnpinButton}
+        />
+      ) : (
+        <GridViewList
+          deckData={deckData}
+          pinnedList={pinnedList}
+          handlePinButton={handlePinButton}
+          unpinnedList={unpinnedList}
+          handleUnpinButton={handleUnpinButton}
+        />
       )}
-    </>
+    </div>
   );
 };
 
