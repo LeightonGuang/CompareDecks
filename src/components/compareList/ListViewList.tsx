9a6c4960ce4f202257c6a6a87e28256986634085
LeightonGuang <leighton.guang@icloud.com>
2025-01-08 12:30:28 +0000

    Feat: Add horizontal padding to table cell

--- src/components/compareList/ListViewList.tsx
+++ src/components/compareList/ListViewList.tsx
@@ -51,7 +51,7 @@ const ListViewList = ({
         <td className="px-[1rem]">
           <div className="flex max-h-[4rem] w-[6rem] justify-center bg-black">
             <img
-              className="h-[4rem] object-contain"
+              className="h-16 object-contain"
               src={card.imgUrl}
               alt={"card image"}
               id="item-image"
@@ -60,7 +60,9 @@ const ListViewList = ({
         </td>
 
         {card.attribute_values.map((attribute, attributeIndex) => (
-          <td key={attributeIndex}>{attribute.value}</td>
+          <td key={attributeIndex} className="px-4">
+            {attribute.value}
+          </td>
         ))}
 
         <td className="whitespace-nowrap px-[1rem]">
