34dad5a9a085875a2bc87855e53a2a790d384734
LeightonGuang <leighton.guang@icloud.com>
2025-01-09 01:07:43 +0000

    Feat: Clean up code layout and use deck data state using props

--- src/components/createDeck/SetupCreateDeckModal.tsx
+++ src/components/createDeck/SetupCreateDeckModal.tsx
@@ -1,134 +1,172 @@
 "use client";
 
-import { useState } from "react";
-import binIcon from "@/_assets/icons/binIcon.svg";
-import Image from "next/image";
-import { useDeck } from "@/context/DeckContext";
-import { DeckAttributesTableType } from "@/_types/DeckAttributesTableType";
-import { CardTableType } from "@/_types/CardsTableType";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import binIcon from "@/_assets/icons/binIcon.svg";
+
+import { DecksTableType } from "@/_types/DecksTableType";
+import { DeckAttributesTableType } from "@/_types/DeckAttributesTableType";
 
 interface Props {
-  setShowSetupCreateDeckModal: (arg0: boolean) => void;
+  setShowCreateDeckModal: (arg0: boolean) => void;
+  deckData: DecksTableType | undefined;
+  setDeckData: (deckData: DecksTableType) => void;
 }
-const SetupCreateDeckModal = ({ setShowSetupCreateDeckModal }: Props) => {
-  const {
-    attributeNames,
-    setAttributeNames,
-    pendingDeckData,
-    setPendingDeckData,
-  } = useDeck();
+
+interface SetupFormType {
+  name: string;
+  deck_attributes: string[];
+}
+
+const SetupCreateDeckModal = ({
+  setShowCreateDeckModal,
+  deckData,
+  setDeckData,
+}: Props) => {
   const router = useRouter();
-  const [deckName, setDeckName] = useState("");
-  const [attributeList, setAttributeList] = useState<string[]>([]);
 
-  const handleDeckNameOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setDeckName(e.target.value);
-  };
+  const SetupForm = () => {
+    const [formData, setFormData] = useState<SetupFormType>({
+      name: "",
+      deck_attributes: [],
+    });
+    const [newAttribute, setNewAttribute] = useState<string>("");
 
-  const handleAddAttributeToList = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const attribute = formData.get("attribute") as string;
-    if (attribute.trim() === "") return;
-    setAttributeList([...attributeList, attribute]);
-    e.currentTarget.reset();
-  };
+    const onDeckNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const name = e.target.value;
+      setFormData({ ...formData, name });
+    };
 
-  const handleRemoveAttribute = (index: number) => {
-    setAttributeList(attributeList.filter((_, i) => i !== index));
-  };
+    const onNewAttributeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const attribute = e.target.value;
+      setNewAttribute(attribute);
+    };
 
-  const handleDeckSetupDone = () => {
-    if (deckName === "") {
-      alert("Please enter a deck name");
-    } else if (attributeList.length === 0) {
-      alert("Please add at least one attribute");
-    } else {
-      // TODO set the attribute list in pending deck data
-      setPendingDeckData({ ...pendingDeckData, name: deckName });
-      const newAttrNames = attributeList.map((attr) => ({
-        id: 0,
-        deck_uuid: "",
-        attribute: attr,
-        created_at: "",
-        edited_at: "",
-      }));
-      console.log("newAttrNames", newAttrNames);
-      setAttributeNames(newAttrNames);
-      setShowSetupCreateDeckModal(false);
-    }
-  };
-  return (
-    <div
-      className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50"
-      id="setup-create-deck-modal"
-    >
-      <div
-        className="w-[16rem] rounded-[0.375rem] bg-white p-[1rem] md:w-[28rem]"
-        id="setup-create-deck-card"
-      >
+    const handleAddAttributeButton = () => {
+      if (newAttribute === "") {
+        alert("Please enter an attribute");
+      }
+      {
+        setFormData({
+          ...formData,
+          deck_attributes: [...formData.deck_attributes, newAttribute],
+        });
+        setNewAttribute("");
+      }
+    };
+
+    const handleRemoveAttributeButton = (index: number) => {
+      const filteredAttributeList = formData.deck_attributes.filter(
+        (_a, aIndex) => aIndex !== index,
+      );
+
+      setFormData({ ...formData, deck_attributes: filteredAttributeList });
+    };
+
+    const handleDoneButton = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (formData.name === "") {
+        alert("Please enter a deck name");
+      } else if (formData.deck_attributes.length === 0) {
+        alert("Please add at least one attribute");
+      } else {
+        const formattedAttributes: DeckAttributesTableType[] =
+          formData.deck_attributes.map((attr, attrIndex) => ({
+            id: undefined,
+            order: attrIndex + 1,
+            deck_uuid: "",
+            attribute: attr,
+            created_at: undefined,
+            edited_at: undefined,
+          }));
+
+        setDeckData({
+          ...deckData,
+          id: undefined,
+          name: formData.name,
+          deck_attributes: formattedAttributes,
+        });
+        setShowCreateDeckModal(false);
+      }
+    };
+
+    useEffect(() => {
+      console.log(formData);
+    }, [formData]);
+
+    return (
+      <form className="flex flex-col gap-4" onSubmit={handleDoneButton}>
         <input
-          className="border-E2E8F0 rounded-md border px-[0.75rem] py-[0.5rem] text-[0.875rem]"
+          className="rounded-md border border-[E2E8F0] px-3 py-2 text-sm"
           type="text"
           placeholder="Deck Name"
-          name="deckName"
-          value={deckName}
-          onChange={handleDeckNameOnChange}
+          value={formData.name}
+          onChange={onDeckNameChange}
         />
-        <p className="my-[1rem] text-[0.875rem] text-[#5E6D82]">
-          {`Setup your deck name and card attributes here.`}
-        </p>
-        <div
-          className="my-[1rem] flex flex-col gap-[1rem]"
-          id="setup-create-deck-card-attribute-list"
-        >
-          {attributeList.map((attr, index) => (
-            <div className="flex justify-between" key={index}>
-              <p>{attr}</p>
-              <button
-                className="flex h-[2rem] w-[2rem] items-center justify-center rounded-[0.5rem] bg-red-500"
-                onClick={() => handleRemoveAttribute(index)}
-              >
-                <Image
-                  className="h-[1rem] w-[1rem]"
-                  src={binIcon}
-                  alt="bin icon"
-                />
-              </button>
-            </div>
-          ))}
-          <form
-            className="flex items-center gap-[1rem]"
-            onSubmit={handleAddAttributeToList}
+
+        <ul className="flex flex-col gap-2">
+          {formData.deck_attributes.length > 0 &&
+            formData.deck_attributes.map((attr, index) => (
+              <li className="flex justify-between" key={index}>
+                <p>{attr}</p>
+                <button
+                  type="button"
+                  className="flex h-8 w-8 items-center justify-center rounded-lg bg-red-500"
+                  onClick={() => handleRemoveAttributeButton(index)}
+                >
+                  <Image className="h-4 w-4" src={binIcon} alt="bin icon" />
+                </button>
+              </li>
+            ))}
+        </ul>
+
+        <div className="flex justify-between">
+          <input
+            className="gap-2 rounded-md border border-[E2E8F0] px-[0.75rem] py-2 text-sm"
+            type="text"
+            placeholder="Add a new attribute"
+            value={newAttribute}
+            onChange={onNewAttributeChange}
+          />
+          <button
+            className="flex min-h-8 min-w-8 items-center justify-center rounded-lg bg-blue text-white"
+            type="button"
+            onClick={handleAddAttributeButton}
           >
-            <input
-              className="border-E2E8F0 w-full rounded-md border px-[0.75rem] py-[0.5rem] text-[0.875rem]"
-              type="text"
-              placeholder="Add a new attribute"
-              name="attribute"
-            />
-            <button
-              className="flex min-h-[2rem] min-w-[2rem] items-center justify-center rounded-[0.5rem] bg-blue text-white"
-              type="submit"
-            >
-              +
-            </button>
-          </form>
+            +
+          </button>
         </div>
+
         <div className="flex gap-2">
           <button
-            className="w-full rounded-md bg-red-600 py-[0.5rem] text-[0.875rem] text-white"
+            type="button"
+            className="w-full rounded-md bg-red-600 py-2 text-sm text-white"
             onClick={() => router.push("/")}
           >
             Cancel
           </button>
           <button
-            className="w-full rounded-md bg-[#3A4FE0] py-[0.5rem] text-[0.875rem] text-white"
-            onClick={handleDeckSetupDone}
+            type="submit"
+            className="w-full rounded-md bg-[#3A4FE0] py-2 text-sm text-white"
           >
             Done
           </button>
+        </div>
+      </form>
+    );
+  };
+
+  return (
+    <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
+      <div className="w-64 rounded-md bg-white p-4 md:w-[28rem]">
+        <p className="my-4 text-sm text-[#5E6D82]">
+          Setup your deck name and card attributes here.
+        </p>
+
+        <div className="my-4 flex flex-col gap-4">
+          <SetupForm />
         </div>
       </div>
     </div>
