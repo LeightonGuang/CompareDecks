144f1a3e53c667570d9226ceffd8d86d48dc872f
LeightonGuang <leighton.guang@icloud.com>
2025-01-11 01:30:01 +0000

    Feat: Migrate away from useContext

--- src/app/(routes)/create-deck/page.tsx
+++ src/app/(routes)/create-deck/page.tsx
@@ -1,111 +1,41 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { useUser } from "@/context/UserContext";
-import { useDeck } from "@/context/DeckContext";
-import CompareList from "@/components/compareList/CompareList";
-
 import { DecksTableType } from "@/_types/DecksTableType";
+import CompareList from "@/components/compareList/CompareList";
 import SetupCreateDeckModal from "@/components/createDeck/SetupCreateDeckModal";
-import { DeckAttributesTableType } from "@/_types/DeckAttributesTableType";
+import React, { useEffect, useState } from "react";
 
 const CreateDeckPage = () => {
-  const router = useRouter();
-  const { user, fetchUser } = useUser();
-  const {
-    setAttributeNames,
-    setOriginalDeckData,
-    pendingDeckData,
-    setPendingDeckData,
-    pinnedList,
-    setPinnedList,
-    unpinnedList,
-    setUnpinnedList,
-    createDeck,
-  } = useDeck();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [newAttributes, setNewAttributes] = useState<DeckAttributesTableType[]>(
-    [],
-  );
-  const [showSetupCreateDeckModal, setShowSetupCreateDeckModal] =
-    useState<boolean>(true);
-
-  const handleCreateDeckButton = async () => {
-    try {
-      if (user.aud !== "authenticated") {
-        return console.error("User is not authenticated");
-      }
-
-      const filteredCardsData = pendingDeckData.cards.map((card) => {
-        const { id, created_at, edited_at, ...rest } = card;
-        return rest;
-      });
-
-      const response = await createDeck(pendingDeckData, newAttributes);
-
-      if (response.success) {
-        router.push(`/decks/${response.deck_uuid}`);
-      } else if (!response.success) {
-        router.push("/error");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleDeckSetup = () => {};
+  const [isLoading, setIsLoading] = useState(true);
+  const [deckData, setDeckData] = useState<DecksTableType>({
+    name: "",
+    user_uid: "",
+    uuid: "",
+    deck_attributes: [],
+    cards: [],
+  });
+  const [showCreateDeckModal, setShowCreateDeckModal] = useState(true);
 
   useEffect(() => {
-    if (pinnedList.length !== 0) setPinnedList([]);
-    if (unpinnedList.length !== 0) setUnpinnedList([]);
-    fetchUser();
-  }, []);
-
-  useEffect(() => {
-    if (user !== null) {
-      setOriginalDeckData({
-        name: "",
-        cards: [],
-        user_uid: user?.id,
-      } as unknown as DecksTableType);
-      setPendingDeckData({
-        name: "",
-        cards: [],
-        user_uid: user?.id,
-      } as unknown as DecksTableType);
-      setIsLoading(false);
-    }
-  }, [user]);
+    console.log("deckData", deckData);
+  }, [deckData]);
 
   return (
-    <main className="h-dynamic-vh overflow-y-auto" id="create-deck-page">
-      <div className="mx-mobile-spacing">
-        {!isLoading && (
-          <>
-            <CompareList />
-            <div className="absolute right-0">
-              <button
-                className="ml-[auto] mr-[1rem] mt-[1rem] rounded-[0.325rem] bg-green-500 px-[1rem] py-[0.5rem] text-white disabled:bg-gray-400 disabled:text-black"
-                onClick={handleCreateDeckButton}
-                disabled={
-                  pendingDeckData?.cards?.length === 0 ||
-                  pendingDeckData?.name === ""
-                }
-              >
-                Create
-              </button>
-            </div>
-          </>
-        )}
-      </div>
+    <section>
+      <CompareList
+        className="mx-4 mt-4"
+        deckData={deckData}
+        setDeckData={setDeckData}
+      />
 
-      {showSetupCreateDeckModal && (
+      {showCreateDeckModal && (
         <SetupCreateDeckModal
-          setShowSetupCreateDeckModal={setShowSetupCreateDeckModal}
+          setShowCreateDeckModal={setShowCreateDeckModal}
+          deckData={deckData}
+          setDeckData={setDeckData}
         />
       )}
-    </main>
+    </section>
   );
 };
 
