9be59eaca797a3363dc7faaa9a6a6cec263601f7
LeightonGuang <leighton.guang@icloud.com>
2025-04-18 02:53:54 +0100

    Fix: Foreign key id comments

--- src/_types/AttributeValuesTableType.ts
+++ src/_types/AttributeValuesTableType.ts
@@ -1,6 +1,6 @@
 export interface AttributeValuesTableType {
   id?: number; //	Unique ID for the value row
-  attribute_id?: number; // Foreign key to Cards(id)
-  card_id?: number; // Foreign key to Attributes(id)
+  attribute_id?: number; // Foreign key to Attributes(id)
+  card_id?: number; // Foreign key to Cards(id)
   value: string; // Attribute value stored as string
 }
