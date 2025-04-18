5a92f9f6993f28366d4e561d3d7e9497bf82c76a
LeightonGuang <leighton.guang@icloud.com>
2025-04-18 02:54:17 +0100

    Feat: Add description to decks table

--- src/_types/DecksTableType.ts
+++ src/_types/DecksTableType.ts
@@ -2,6 +2,7 @@ export interface DecksTableType {
   id?: number; // Unique ID for the deck row
   uuid?: string; // Public identifier for the deck
   name?: string; // Name of the deck
+  description?: string; // Description of the deck
   user_uid?: string; // Foreign key to Users(uid)
   created_at?: Date; // Creation date
   edited_at?: Date; // Last edit date
