4a374f7ae74c1d6769c3033d628ddd0eae0087ae
LeightonGuang <leighton.guang@icloud.com>
2025-04-21 19:42:02 +0100

    Fix: Add missing description column

--- src/_types/CardsTableType.ts
+++ src/_types/CardsTableType.ts
@@ -2,6 +2,7 @@ export interface CardTableType {
   id?: number; // Unique ID for the card row
   deck_uuid?: string; // Foreign key to Decks(uuid)
   name: string; // Name of the product
-  card_order: number; // Order of the card in the deck
   imgUrl?: string; // URL of the image
+  description?: string; // Description of the product
+  card_order: number; // Order of the card in the deck
 }
