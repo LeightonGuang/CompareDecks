a3d085099c034b42cb0cfec9be80aa9cdd18a819
LeightonGuang <leighton.guang@icloud.com>
2025-04-22 02:45:44 +0100

    Fix: Supabase query for new database table structure

--- src/app/actions/DeckContext/getDeckById/actions.ts
+++ src/app/actions/DeckContext/getDeckById/actions.ts
@@ -1,22 +1,23 @@
 "use server";
-import { DecksTableType } from "@/_types/DecksTableType";
 import { getSupabaseServer } from "@/utils/supabase/server";
+
+import { FetchDeckDataType } from "@/_types/FetchDeckDataType";
 
 export async function getDeckById(
   uuid: string,
-): Promise<{ data: DecksTableType | null; error: any }> {
+): Promise<{ data: FetchDeckDataType | null; error: any }> {
   try {
     const supabaseServer = getSupabaseServer();
     const { data, error } = await supabaseServer
       .from("decks")
       .select(
         `*,
-        deck_attributes (*),
+        attributes (*),
         cards (
           *,
           attribute_values (
             *,
-            deck_attributes (
+            attributes (
               *
             )
           )
