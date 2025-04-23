d4bf4811e0add7b3dd0ea82e1a365d12090a1633
LeightonGuang <leighton.guang@icloud.com>
2025-04-23 03:42:16 +0100

    Refactor: Update supabase query for list of decks

--- src/app/actions/DeckContext/getAllDecksList/actions.ts
+++ src/app/actions/DeckContext/getAllDecksList/actions.ts
@@ -1,38 +1,40 @@
 "use server";
+import { DeckListType } from "@/_types/DeckListType";
 import { getSupabaseServer } from "@/utils/supabase/server";
 
-/**
- * Get list of all decks
- *
- * @returns {Object}
- * @property {DeckType[] | null} allDecksList - list of all decks
- * @property {string} error - error message
- */
+// Get list of all decks
 
-export async function getAllDecksList() {
-  try {
-    const supabase = getSupabaseServer();
-    const { data, error } = await supabase
-      .from("decks")
-      .select(`id, user_uid, uuid, name, created_at, edited_at , cards(imgUrl)`)
-      .order("id", { ascending: true });
+export async function getAllDecksList(): Promise<{
+  data: DeckListType[] | null;
+  error: any | null;
+}> {
+  {
+    try {
+      const supabase = getSupabaseServer();
+      const { data, error } = await supabase
+        .from("decks")
+        .select(
+          `id, user_uid, uuid, name, created_at, edited_at , cards(imgUrl)`,
+        )
+        .order("id", { ascending: true });
 
-    if (data) {
-      return { allDecksList: data, error: null };
+      if (data) {
+        return { data: data, error: null };
+      }
+
+      if (error) {
+        console.error("Error fetching decks:", error);
+        return { data: null, error: error };
+      }
+      if (!data) {
+        console.error("No decks found:", error);
+        return { data: null, error: "No decks found" };
+      }
+
+      return { data: data, error: null };
+    } catch (error) {
+      console.error("Unexpected error:", error);
+      return { data: null, error: "Unexpected error" };
     }
-
-    if (error) {
-      console.error("Error fetching decks:", error.message);
-      return { allDecksList: null, error: error.message };
-    }
-    if (!data) {
-      console.error("No decks found:", error);
-      return { allDecksList: null, error: "No decks found" };
-    }
-
-    return { allDecksList: data, error: null };
-  } catch (error) {
-    console.error("Unexpected error:", error);
-    return { allDecksList: null, error: "Unexpected error" };
   }
 }
